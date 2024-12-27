52e47cdc1d6c5fd6c203d77fdb9d89cbf66e1159
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2024-12-27 14:51:51 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,18 +1,75 @@
-FAILURE: Build failed with an exception.
-* What went wrong:
-Execution failed for task ':compileJava'.
-> Could not resolve all files for configuration ':compileClasspath'.
-   > Could not resolve org.springframework.boot:spring-boot-starter-webflux:3.3.3.
-     Required by:
-         project :
-      > Could not resolve org.springframework.boot:spring-boot-starter-webflux:3.3.3.
-         > Could not get resource 'https://repo.maven.apache.org/maven2/org/springframework/boot/spring-boot-starter-webflux/3.3.3/spring-boot-starter-webflux-3.3.3.pom'.
-            > Could not GET 'https://repo.maven.apache.org/maven2/org/springframework/boot/spring-boot-starter-webflux/3.3.3/spring-boot-starter-webflux-3.3.3.pom'.
-               > Got SSL handshake exception during request. It might be caused by SSL misconfiguration
-                  > PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException: unable to find valid certification path to requested target
-* Try:
-> Run with --stacktrace option to get the stack trace.
-> Run with --info or --debug option to get more log output.
-> Run with --scan to get full insights.
-> Get more help at https://help.gradle.org.
-BUILD FAILED in 3s
+public void validatePayment(BigDecimal transactionAmount, String mId, String paymodeCode) {
+        logger.info("Fetching data from Admin Service : {}, {}", mId, paymodeCode);
+        AggMerchantVolumeVelocityResponse paymodeVVL = getPaymodeVVL(mId, paymodeCode);
+
+        if (ObjectUtils.isEmpty(paymodeVVL)) {
+            throw new ValidationException(ErrorConstants.NOT_FOUND_ERROR_CODE, MessageFormat.format(ErrorConstants.NOT_FOUND_ERROR_MESSAGE, "PayMode VVL"));
+        }
+
+        MerchantOrderSummary merchantOrderSummary = getMerchantOrderSummary(mId, paymodeCode);
+
+        logger.info("Validation Started.");
+        if (transactionAmount.compareTo(paymodeVVL.minTxnLimit) < 0 || transactionAmount.compareTo(paymodeVVL.maxTxnLimit) > 0) {
+
+            throw new ValidationException(ErrorConstants.PAYMENT_TRANSACTION_LIMIT_CODE, ErrorConstants.PAYMENT_LIMIT_ERROR_MESSAGE);
+        }
+
+        if (paymodeVVL.dailyTxnAmountLimit.compareTo(transactionAmount.add(merchantOrderSummary.getDailyTxnAmount())) < 0) {
+
+            throw new ValidationException(ErrorConstants.PAYMENT_DAILY_TRANSACTION_LIMIT_CODE, ErrorConstants.PAYMENT_DAILY_LIMIT_ERROR_MESSAGE);
+        }
+
+        if (paymodeVVL.weeklyTxnAmountLimit.compareTo(transactionAmount.add(merchantOrderSummary.getWeeklyTxnAmount())) < 0) {
+
+            throw new ValidationException(ErrorConstants.PAYMENT_WEEKLY_TRANSACTION_LIMIT_CODE, ErrorConstants.PAYMENT_WEEKLY_LIMIT_ERROR_MESSAGE);
+        }
+
+        if (paymodeVVL.monthlyTxnAmountLimit.compareTo(transactionAmount.add(merchantOrderSummary.getMonthlyTxnAmount())) < 0) {
+
+            throw new ValidationException(ErrorConstants.PAYMENT_MONTHLY_TRANSACTION_LIMIT_CODE, ErrorConstants.PAYMENT_MONTHLY_LIMIT_ERROR_MESSAGE);
+        }
+
+        if (paymodeVVL.quaterlyTxnAmountLimit.compareTo(transactionAmount.add(merchantOrderSummary.getQuarterlyTxnAmount())) < 0) {
+
+            throw new ValidationException(ErrorConstants.PAYMENT_QUARTERLY_TRANSACTION_LIMIT_CODE, ErrorConstants.PAYMENT_QUARTERLY_LIMIT_ERROR_MESSAGE);
+        }
+
+        if (paymodeVVL.halfYearlyTxnAmountLimit.compareTo(transactionAmount.add(merchantOrderSummary.getHalfyearlyTxnAmount())) < 0) {
+
+            throw new ValidationException(ErrorConstants.PAYMENT_HALFYEARLY_TRANSACTION_LIMIT_CODE, ErrorConstants.PAYMENT_HALFYEARLY_LIMIT_ERROR_MESSAGE);
+        }
+
+        if (paymodeVVL.annualTxnAmountLimit.compareTo(transactionAmount.add(merchantOrderSummary.getAnnuallyTxnAmount())) < 0) {
+
+            throw new ValidationException(ErrorConstants.PAYMENT_ANNUAL_TRANSACTION_LIMIT_CODE, ErrorConstants.PAYMENT_ANNUAL_LIMIT_ERROR_MESSAGE);
+        }
+        logger.info("Validation Completed.");
+
+    }
+
+    private MerchantOrderSummary getMerchantOrderSummary(String mId, String paymodeCode) {
+        logger.info("Getting Merchant Order Summery from data base by : {},{}", mId, paymodeCode);
+        List<MerchantOrderSummary> orderSummary = merchantOrderSummaryDao.getMerchantOrderSummary(mId, paymodeCode);
+
+        if (orderSummary.isEmpty()) {
+            throw new ValidationException(ErrorConstants.NOT_FOUND_ERROR_CODE, MessageFormat.format(ErrorConstants.NOT_FOUND_ERROR_MESSAGE, "Order Summary"));
+        }
+
+        return orderSummary.getFirst();
+    }
+
+    private AggMerchantVolumeVelocityResponse getPaymodeVVL(String mId, String paymodeCode) {
+        logger.info("Calling geVvlDetails.");
+        List<AggMerchantVolumeVelocityResponse> vvlDetailsList = AdminServicesClient.geVvlDetails(mId, webClientbuilder);
+        AggMerchantVolumeVelocityResponse paymodeVVL = null;
+        logger.info("Adding VVL Details by PayModeCode.");
+        for (AggMerchantVolumeVelocityResponse aggMerchantVolumeVelocityResponse : vvlDetailsList) {
+
+            if (aggMerchantVolumeVelocityResponse.paymodeCode.equalsIgnoreCase(paymodeCode)) {
+                paymodeVVL = aggMerchantVolumeVelocityResponse;
+                break;
+            }
+        }
+
+        return paymodeVVL;
+    }
