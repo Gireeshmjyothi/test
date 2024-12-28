646dd79e0ded092e82d6dcc782c4507f2d02e332
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2024-12-28 12:54:37 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,49 +1,19 @@
-private String encryptPaymentVerificationResponse(PaymentVerificationResponse paymentVerificationResponse, MerchantInfoDTO merchantDto) {
-        try {
-            return encryptionDecryptionUtil.encryptRequest(objectMapper.writeValueAsString(paymentVerificationResponse), merchantDto);
-        } catch (JsonProcessingException e) {
-            logger.error("Error in encryptPaymentVerificationData ", e.getMessage());
-            throw new TransactionException(ErrorConstants.INVALID_ERROR_CODE, MessageFormat.format(ErrorConstants.INVALID_ERROR_MESSAGE, "paymentVerificationResponse", "Error occurred while encrypting the payment verification response."));
+ private static boolean validateAndMatch(Character a, Character b) {
+
+        if (a == null || b == null) {
+            return false;
         }
+
+        return Character.toUpperCase(a) == Character.toUpperCase(b);
+
     }
 
+    private static boolean validateAndMatch(String a, String b) {
 
+        if (a == null || b == null) {
+            return false;
+        }
 
- @ExceptionHandler(value = {JwtException.class})
-    public ResponseEntity<Object> handleJwtException(JwtException ex, WebRequest request) {
-        String requestUri = ((ServletWebRequest)request).getRequest().getRequestURI();
-        logger.error("Error in Authorization ", ex);
-        ErrorDto errorDto = ErrorDto.builder()
-                .errorCode(String.valueOf(HttpStatus.UNAUTHORIZED.value()))
-                .errorMessage(ex.getMessage())
-                .build();
+        return a.equalsIgnoreCase(b);
 
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
-                TransactionResponse.builder()
-                        .status(0)
-                        .errors(List.of(errorDto))
-                        .build());
     }
-
-@ExceptionHandler(value = {JsonProcessingException.class})
-public ResponseEntity<Object> handleJsonProcessingException(JsonProcessingException ex, WebRequest request) {
-    String requestUri = ((ServletWebRequest) request).getRequest().getRequestURI();
-    logger.error("JSON Processing Exception occurred at URI: {}", requestUri, ex);
-
-    // Create an error DTO
-    ErrorDto errorDto = ErrorDto.builder()
-            .errorCode(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-            .errorMessage("Error occurred while processing JSON data: " + ex.getOriginalMessage())
-            .build();
-
-    // Return a custom response
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
-            TransactionResponse.builder()
-                    .status(0)
-                    .errors(List.of(errorDto))
-                    .build()
-    );
-}
-private String encryptPaymentVerificationResponse(PaymentVerificationResponse paymentVerificationResponse, MerchantInfoDTO merchantDto) {
-    return encryptionDecryptionUtil.encryptRequest(objectMapper.writeValueAsString(paymentVerificationResponse), merchantDto);
-}
