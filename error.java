7c23199bc95963687efa27374bacefe16c596513
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2024-12-28 11:44:50 +0530

    Update error.java

--- error.java
+++ error.java
@@ -6,3 +6,21 @@ private String encryptPaymentVerificationResponse(PaymentVerificationResponse pa
             throw new TransactionException(ErrorConstants.INVALID_ERROR_CODE, MessageFormat.format(ErrorConstants.INVALID_ERROR_MESSAGE, "paymentVerificationResponse", "Error occurred while encrypting the payment verification response."));
         }
     }
+
+
+
+ @ExceptionHandler(value = {JwtException.class})
+    public ResponseEntity<Object> handleJwtException(JwtException ex, WebRequest request) {
+        String requestUri = ((ServletWebRequest)request).getRequest().getRequestURI();
+        logger.error("Error in Authorization ", ex);
+        ErrorDto errorDto = ErrorDto.builder()
+                .errorCode(String.valueOf(HttpStatus.UNAUTHORIZED.value()))
+                .errorMessage(ex.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+                TransactionResponse.builder()
+                        .status(0)
+                        .errors(List.of(errorDto))
+                        .build());
+    }
