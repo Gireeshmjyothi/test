417182ab821c5968cbcd39fde938ede79800ac4d
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-01-04 15:55:45 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,16 +1,69 @@
-CREATE TABLE ERROR_LOG(
-    ID                      RAW(16) DEFAULT SYS_GUID() PRIMARY KEY NOT NULL,
-    MID                     VARCHAR2(50) UNIQUE,
-    ORDER_REF_NUMBER        VARCHAR2(50),
-    SBI_ORDER_REF_NUMBER    VARCHAR2(50),
-    ATRN                    VARCHAR2(50),
-    ENTITY                  VARCHAR2(50) NOT NULL,
-    PAY_MODE                VARCHAR2(50) NOT NULL,
-    ERROR_CODE              VARCHAR2(20) NOT NULL,
-    FAILURE_REASON          VARCHAR2(50),
-    ERROR_MESSAGE           VARCHAR2(1000),
-    CREATED_BY              VARCHAR2(50) NOT NULL,
-    CREATED_AT              NUMBER NOT NULL,
-    UPDATED_BY              VARCHAR2(50) NOT NULL,
-    UPDATED_AT              NUMBER NOT NULL
-);
+@Repository
+@RequiredArgsConstructor
+public class ErrorLogDao {
+    private static final LoggerUtility logger = LoggerFactoryUtility.getLogger(ErrorLogDao.class);
+
+    private final ErrorLogRepository errorLogRepository;
+    private final ObjectMapper objectMapper;
+
+    public void saveErrorLog(ErrorLogDto errorLogDto) {
+        logger.info("Saving Error Log : {}", errorLogDto);
+        errorLogRepository.save(objectMapper.convertValue(errorLogDto, ErrorLog.class));
+        logger.info("Error Log Saved Successfully.");
+    }
+}
+
+
+@Data
+@Builder
+public class ErrorLogDto {
+    private String mID;
+    private String orderRefNumber;
+    private String sbiOrderRefNumber;
+    private String atrn;
+
+    @Enumerated(EnumType.STRING)
+    private EntityType entityType;
+
+    @Enumerated(EnumType.STRING)
+    private PayMode payMode;
+
+    @Enumerated(EnumType.STRING)
+    private FailureReason failureReason;
+
+    private String errorCode;
+    private String errorMessage;
+}
+
+@EqualsAndHashCode(callSuper = true)
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "ERROR_LOG")
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ErrorLog extends AuditEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false, updatable = false, unique = true)
+    private UUID id;
+
+    private String mID;
+    private String orderRefNumber;
+    private String sbiOrderRefNumber;
+    private String atrn;
+
+    @Enumerated(EnumType.STRING)
+    private EntityType entityType;
+
+    @Enumerated(EnumType.STRING)
+    private PayMode payMode;
+
+    @Enumerated(EnumType.STRING)
+    private FailureReason failureReason;
+
+    private String errorCode;
+    private String errorMessage;
+}
