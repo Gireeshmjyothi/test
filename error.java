e29b82e7922d767d31ad149d22e5fec6731e0732
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-01-10 19:29:55 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,8 +1,7 @@
-@Query("SELECT t FROM Transaction t " +
-       "WHERE t.mid = :mid " +
-       "AND t.date >= :startDate " +
-       "AND t.date <= :endDate " +
-       "ORDER BY t.date")
+@Query(value = "SELECT * FROM transaction t " +
+               "WHERE t.mid = :mid " +
+               "AND t.date BETWEEN :startDate AND :endDate " +
+               "ORDER BY t.date", nativeQuery = true)
 List<Transaction> findTransactionsByMidAndDateRange(@Param("mid") String mid,
                                                     @Param("startDate") Date startDate,
                                                     @Param("endDate") Date endDate);
