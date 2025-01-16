3ed9ffc6c31f104927a69b3a8dd30531762f5e10
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-01-16 12:17:24 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,78 +1,21 @@
-@Repository
-public interface BankRepository extends JpaRepository<BankBranchView, String> {
+@Scheduled(cron = "${scheduler.cron.expression}")
+    @SchedulerLock(name = "TransactionService_checkExpiredToken",lockAtLeastFor = "${scheduler.lockAtLeastFor}",lockAtMostFor = "${scheduler.lockAtMostFor}")
+    public void checkExpiredToken() {
+        logger.info("Token Expiry scheduler start ");
+        Long currentTimeStamp = Instant.now().toEpochMilli();
+        List<Token> expiredToken = tokenRepository.findByStatusAndTokenExpiryTimeLessThan(TokenStatus.ACTIVE, currentTimeStamp);
+        expiredToken.forEach(token->
+        {
+            tokenDao.saveToken(buildTokenDtoForInvalidate(objectMapper.convertValue(token, TokenDto.class)));
+         });
+        logger.info("Token Expiry scheduler end ");
+    }
 
-    @Query(value = """
-            SELECT bb, bm
-            FROM BankBranchView bb
-            INNER JOIN BankMasterView bm
-            ON bb.bankId = bm.bankId
-            WHERE bb.ifscCode = :ifsccode
-            """)
-    Optional<List<Object[]>> fetchBankBranchAndMasterByIfscCode(@Param("ifsccode") String ifsccode);
-}
-@Data
-@Entity
-@Table(name = "BANK_MASTER_VIEW")
-public class BankMasterView {
-
-    @Id
-    private String bankId;
-    @Column(name = "BANKNAME")
-    private String bankname;
-    private Long creationDate;
-    private String createdBy;
-    private Long modifiedDate;
-    private String modifiedBy;
-    private String status;
-    private String bankAbbreviation;
-    private String bankName;
-
-}  
-
-@Data
-@Entity
-@Table(name = "BANK_BRANCHES_VIEW")
-public class BankBranchView {
-    @Id
-    private String branchId;
-    private String bankId;
-    private String branchName;
-    private String ifscCode;
-    private String branchAddress;
-    private String city;
-    private String state;
-    private char status;
-    private String remarks;
-    private String remarks1;
-    private String checkerRemark;
-    private String checkerRemark1;
-
-    private Date creationDate;
-    private String createdBy;
-    private Date modifiedDate;
-    private String modifiedBy;
-
-}
-
-@Query(value = """
-                SELECT t, o
-                FROM Transaction t
-                INNER JOIN Order o
-                ON t.orderRefNumber = o.orderRefNumber
-                WHERE t.atrnNumber = :atrnNumber
-                AND (:orderRefNumber IS NULL OR t.orderRefNumber = :orderRefNumber)
-                AND (:sbiOrderRefNumber IS NULL OR t.sbiOrderRefNumber = :sbiOrderRefNumber)
-                AND (:orderAmount IS NULL OR o.orderAmount = :orderAmount)
-            """)
-    Optional<List<Object[]>> fetchTransactionAndOrderDetail(
-            @Param("atrnNumber") String atrnNumber,
-            @Param("orderRefNumber") String orderRefNumber,
-            @Param("sbiOrderRefNumber") String sbiOrderRefNumber,
-            @Param("orderAmount") BigDecimal orderAmount);
-
-
-@Query(value = "SELECT bm.*, bb.*" +
-            "FROM BANK_MASTER_VIEW bm " +
-            "JOIN BANK_BRANCHES_VIEW bb ON bm.bankid = bb.bank_id " +
-            "WHERE bb.IFSC_CODE = :ifscCode", nativeQuery = true)
-    List<Object[]> findBankMasterAndBranchByIfscCode(@Param("ifscCode") String ifscCode);
+    private TokenDto buildTokenDtoForInvalidate(TokenDto tokenDto) {
+        tokenDto.setTokenValid(Boolean.FALSE);
+        tokenDto.setStatus(TokenStatus.INACTIVE);
+        tokenDto.setExpiredAt(System.currentTimeMillis()) ;
+        tokenDto.setUpdatedAt(System.currentTimeMillis());
+        tokenDto.setUpdatedBy(System.getProperty("user.name"));
+        return tokenDto;
+    }
