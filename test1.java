219559d30540430079f15ffae3e51a1e8380413c
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-01-17 14:54:12 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,11 +1,3 @@
- @Query("SELECT new com.epay.transaction.dto.TokenAndOrderDto(t.merchantId, o.sbiOrderRefNumber, o.status) " +
-            "FROM Token t " +
-            "JOIN Order o " +
-            "ON t.order_hash = o.order_hash" +
-            "WHERE t.tokenType = :tokenType " +
-            "AND t.tokenExpiryTime < :currentDate " +
-            "AND (o.status != 'PAID' OR o.status IS NULL)")
-    List<TokenAndOrderDto> findTokensByTypeExpiryAndOrderStatus(@Param("tokenType") String tokenType,
-                                                                @Param("currentDate") Long currentDate);
+List<TokenAndOrderDto> tokenAndOrderDto = tokenRepository.findTokensByTypeExpiryAndOrderStatus(TokenType.TRANSACTION, currentTimeStamp);
 
-Caused by: org.hibernate.query.SyntaxException: At 1:160 and token 't', mismatched input 't', expecting one of the following tokens: <EOF>, ',', '.', '[', '+', '-', '*', '/', '%', '||', AND, BY, CROSS, DAY, EPOCH, FULL, GROUP, HOUR, INNER, JOIN, LEFT, MINUTE, MONTH, NANOSECOND, OR, ORDER, OUTER, QUARTER, RIGHT, SECOND, WEEK, WHERE, YEAR [SELECT new com.epay.transaction.dto.TokenAndOrderDto(t.merchantId, o.sbiOrderRefNumber, o.status) FROM Token t JOIN Order o ON t.order_hash = o.order_hashWHERE t.tokenType = :tokenType AND t.tokenExpiryTime < :currentDate AND (o.status != 'PAID' OR o.status IS NULL)]
+List<MerchantDVPDto> merchantDVPDtoList = adminServicesClient.getMerchantDVPFlag(merchantIds);
