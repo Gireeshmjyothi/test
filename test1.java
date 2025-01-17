006bac627dab93db55d2afb94c245c9945536247
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-01-17 19:18:23 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -29,3 +29,13 @@ void updateOrderStatusToFailed(@Param("sbiOrderRefNumbers") List<String> sbiOrde
        "AND EXISTS (SELECT 1 FROM Transaction t WHERE t.sbiOrderRefNumber = o.sbiOrderRefNumber " +
        "AND t.transactionStatus = 'BOOKED' AND t.paymentStatus = '')")
 void updateOrderStatusToExpired(@Param("sbiOrderRefNumbers") List<String> sbiOrderRefNumbers);
+
+@Transactional
+@Modifying
+@Query("UPDATE Orders o " +
+       "SET o.status = 'EXPIRED' " +
+       "WHERE o.status = 'CREATED' " +
+       "AND o.sbiOrderRefNumber IN :sbiOrderRefNumbers " +
+       "AND EXISTS (SELECT 1 FROM Transaction t WHERE t.sbiOrderRefNumber = o.sbiOrderRefNumber " +
+       "AND t.transactionStatus = '' AND t.paymentStatus = '')")
+void updateOrderStatusToExpired(@Param("sbiOrderRefNumbers") List<String> sbiOrderRefNumbers);
