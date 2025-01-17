21dd31a17857f062a8edab42cc28f6cd1d44ea7f
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-01-17 16:07:29 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,3 +1,20 @@
-List<TokenAndOrderDto> tokenAndOrderDto = tokenRepository.findTokensByTypeExpiryAndOrderStatus(TokenType.TRANSACTION, currentTimeStamp);
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
-List<MerchantDVPDto> merchantDVPDtoList = adminServicesClient.getMerchantDVPFlag(merchantIds);
+import java.util.List;
+
+public interface OrderTransactionRepository extends CrudRepository<OrderTransaction, Long> {
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE orders o, transaction t " +
+                   "SET o.status = 'FAILED', t.status = 'FAILED' " +
+                   "WHERE o.sbiOrderRefNumber = t.sbiOrderRefNumber " +
+                   "AND o.status = 'ATTEMPTED' " +
+                   "AND t.status = 'BOOKED' " +
+                   "AND t.paymentStatus = 'PAYMENT_INITIATION_START' " +
+                   "AND o.sbiOrderRefNumber IN :orderRefNumbers", nativeQuery = true)
+    int updateOrderAndTransactionStatusToFailed(List<String> orderRefNumbers);
+}
