475f12f508ed2b7f4dd0f4a3b24f3f2d0c42ca6f
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-01-17 13:32:35 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,23 +1,7 @@
-List<Token> findByTokenTypeAndTokenExpiryTimeLessThan(TokenType tokenType, Long currentDate);
-
-@Query("SELECT t FROM Token t JOIN Order o ON t.mid = o.mid " +
-       "WHERE t.tokenType = :tokenType " +
-       "AND t.tokenExpiryTime < :currentDate " +
-       "AND (o.orderStatus != 'PAID' OR o.orderStatus IS NULL)")
-List<Token> findTokensByTypeExpiryAndOrderStatus(@Param("tokenType") TokenType tokenType,
-                                                 @Param("currentDate") Long currentDate);
-@Query(value = "SELECT t.* FROM token t " +
+ @Query(value = "SELECT new com.epay.transaction.dto.TokenAndOrderDto(t.merchantId, o.sbiOrderRefNumber, o.status), FROM token t " +
             "JOIN orders o ON t.order_hash = o.order_hash " +
             "WHERE t.token_type = :tokenType " +
             "AND t.token_expiry_time < :currentDate " +
-            "AND (o.status != 'PAID' OR o.status IS NULL)",
-            nativeQuery = true)
-    List<Token> findTokensByTypeExpiryAndOrderStatus(@Param("tokenType") TokenType tokenType,
-                                                     @Param("currentDate") Long currentDate);
-
-Caused by: org.hibernate.exception.SQLGrammarException: JDBC exception executing SQL [SELECT t.* FROM token t JOIN orders o ON t.order_hash = o.order_hash WHERE t.token_type = ? AND t.token_expiry_time < ? AND (o.status != 'PAID' OR o.status IS NULL)] [ORA-01722: invalid number
-
-    SELECT t.* FROM token t 
-JOIN orders o ON t.order_hash = o.order_hash 
-WHERE t.token_type = :tokenType 
-AND t.token_expiry_time < :currentDate;   
+            "AND (o.status != 'PAID' OR o.status IS NULL)", nativeQuery = true)
+    List<TokenAndOrderDto> findTokensByTypeExpiryAndOrderStatus(@Param("tokenType") String tokenType,
+                                                                @Param("currentDate") Long currentDate);
