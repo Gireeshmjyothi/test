31b808b0b626bd8ec843b7c76efdd57bc7aefbc6
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-01-17 16:52:49 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,18 +1,29 @@
 @Transactional
-@Modifying
-@Query(value = "UPDATE orders o " +
-               "SET o.status = 'FAILED' " +
-               "WHERE o.sbiOrderRefNumber IN :orderRefNumbers " +
-               "AND o.status = 'ATTEMPTED' " +
-               "AND EXISTS (SELECT 1 FROM transaction t WHERE t.sbiOrderRefNumber = o.sbiOrderRefNumber AND t.status = 'BOOKED' AND t.paymentStatus = 'PAYMENT_INITIATION_START')", nativeQuery = true)
-int updateOrderStatusToFailed(List<String> orderRefNumbers);
+    @Modifying
+    @Query("UPDATE Transaction t " +
+            "SET t.transactionStatus = 'FAILED' " +
+            "WHERE t.transactionStatus = 'BOOKED' " +
+            "AND t.sbiOrderRefNumber IN :sbiOrderRefNumber")
+    void updateTransactionStatusToFailed(@Param("sbiOrderRefNumber") List<String> sbiOrderRefNumber);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Orders o, Transaction t " +
+            "SET o.status = 'FAILED', t.transactionStatus = 'FAILED' " +
+            "WHERE o.sbiOrderRefNumber = t.sbiOrderRefNumber " +
+            "AND o.status = 'ATTEMPTED' " +
+            "AND t.transactionStatus = 'BOOKED' " +
+            "AND t.paymentStatus = 'PAYMENT_INITIATED_START' " +
+            "AND o.sbiOrderRefNumber IN :orderRefNumbers")
+    void updateOrderAndTransactionStatusToFailed(List<String> orderRefNumbers);
+
 
 @Transactional
-@Modifying
-@Query(value = "UPDATE transaction t " +
-               "SET t.status = 'FAILED' " +
-               "WHERE t.sbiOrderRefNumber IN :orderRefNumbers " +
-               "AND t.status = 'BOOKED' " +
-               "AND t.paymentStatus = 'PAYMENT_INITIATION_START' " +
-               "AND EXISTS (SELECT 1 FROM orders o WHERE o.sbiOrderRefNumber = t.sbiOrderRefNumber AND o.status = 'ATTEMPTED')", nativeQuery = true)
-int updateTransactionStatusToFailed(List<String> orderRefNumbers);
+    public void updateStatusOfOrderAndTransaction(List<TokenAndOrderDto> tokenAndOrderDtoList){
+        List<String> listOfSbiOrderRefNum = tokenAndOrderDtoList.stream().map(TokenAndOrderDto::getSbiOrderRefNumber).toList();
+        transactionRepository.updateTransactionStatusToFailed(listOfSbiOrderRefNum);
+        transactionRepository.updateOrderAndTransactionStatusToFailed(listOfSbiOrderRefNum);
+    }
+
+
+Caused by: org.hibernate.query.SyntaxException: At 1:15 and token ',', mismatched input ',' expecting SE [UPDATE Orders o, Transaction t SET o.status = 'FAILED', t.transactionStatus = 'FAILED' WHERE o.sbiOrderRefNumber = t.sbiOrderRefNumber AND o.status = 'ATTEMPTED' AND t.transactionStatus = 'BOOKED' AND t.paymentStatus = 'PAYMENT_INITIATED_START' AND o.sbiOrderRefNumber IN :orderRefNumbers]
