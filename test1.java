4ae5a60cb5591082aa4072669c2b6e534821a006
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-01-17 11:39:48 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,1 +1,8 @@
 List<Token> findByTokenTypeAndTokenExpiryTimeLessThan(TokenType tokenType, Long currentDate);
+
+@Query("SELECT t FROM Token t JOIN Order o ON t.mid = o.mid " +
+       "WHERE t.tokenType = :tokenType " +
+       "AND t.tokenExpiryTime < :currentDate " +
+       "AND (o.orderStatus != 'PAID' OR o.orderStatus IS NULL)")
+List<Token> findTokensByTypeExpiryAndOrderStatus(@Param("tokenType") TokenType tokenType,
+                                                 @Param("currentDate") Long currentDate);
