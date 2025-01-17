b25555648a2f6eca2f316f1cddd274bc0e07ac6a
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-01-17 13:47:01 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,26 +1,11 @@
- @Query(value = "SELECT new com.epay.transaction.dto.TokenAndOrderDto(t.merchantId, o.sbiOrderRefNumber, o.status), FROM token t " +
-            "JOIN orders o ON t.order_hash = o.order_hash " +
-            "WHERE t.token_type = :tokenType " +
-            "AND t.token_expiry_time < :currentDate " +
-            "AND (o.status != 'PAID' OR o.status IS NULL)", nativeQuery = true)
+ @Query("SELECT new com.epay.transaction.dto.TokenAndOrderDto(t.merchantId, o.sbiOrderRefNumber, o.status) " +
+            "FROM Token t " +
+            "JOIN Order o " +
+            "ON t.order_hash = o.order_hash" +
+            "WHERE t.tokenType = :tokenType " +
+            "AND t.tokenExpiryTime < :currentDate " +
+            "AND (o.status != 'PAID' OR o.status IS NULL)")
     List<TokenAndOrderDto> findTokensByTypeExpiryAndOrderStatus(@Param("tokenType") String tokenType,
                                                                 @Param("currentDate") Long currentDate);
 
-@Query("SELECT new com.epay.transaction.dto.TokenAndOrderDto(t.merchantId, o.sbiOrderRefNumber, o.status) " +
-       "FROM Token t " +
-       "JOIN t.orders o " +
-       "WHERE t.tokenType = :tokenType " +
-       "AND t.tokenExpiryTime < :currentDate " +
-       "AND (o.status != 'PAID' OR o.status IS NULL)")
-List<TokenAndOrderDto> findTokensByTypeExpiryAndOrderStatus(@Param("tokenType") String tokenType,
-                                                            @Param("currentDate") Long currentDate);
-
-
-@Query(value = "SELECT t.merchant_id as merchantId, o.sbi_order_ref_number as sbiOrderRefNumber, o.status as status " +
-               "FROM token t " +
-               "JOIN orders o ON t.order_hash = o.order_hash " +
-               "WHERE t.token_type = :tokenType " +
-               "AND t.token_expiry_time < :currentDate " +
-               "AND (o.status != 'PAID' OR o.status IS NULL)", nativeQuery = true)
-List<Object[]> findTokensByTypeExpiryAndOrderStatus(@Param("tokenType") String tokenType,
-                                                    @Param("currentDate") Long currentDate);
+Caused by: org.hibernate.query.SyntaxException: At 1:160 and token 't', mismatched input 't', expecting one of the following tokens: <EOF>, ',', '.', '[', '+', '-', '*', '/', '%', '||', AND, BY, CROSS, DAY, EPOCH, FULL, GROUP, HOUR, INNER, JOIN, LEFT, MINUTE, MONTH, NANOSECOND, OR, ORDER, OUTER, QUARTER, RIGHT, SECOND, WEEK, WHERE, YEAR [SELECT new com.epay.transaction.dto.TokenAndOrderDto(t.merchantId, o.sbiOrderRefNumber, o.status) FROM Token t JOIN Order o ON t.order_hash = o.order_hashWHERE t.tokenType = :tokenType AND t.tokenExpiryTime < :currentDate AND (o.status != 'PAID' OR o.status IS NULL)]
