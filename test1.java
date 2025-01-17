a3eb87d259a435f17c2355092409ad15c74d9af5
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-01-17 23:52:27 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -4,8 +4,7 @@
        "SET t.transactionStatus = 'FAILED' " +
        "WHERE t.transactionStatus = 'BOOKED' " +
        "AND t.paymentStatus = 'PAYMENT_INITIATED_START' " +
-       "AND t.sbiOrderRefNumber IN :sbiOrderRefNumbers " +
-       "AND EXISTS (SELECT 1 FROM Orders o WHERE o.sbiOrderRefNumber = t.sbiOrderRefNumber AND o.status = 'ATTEMPTED')")
+       "AND t.sbiOrderRefNumber IN :sbiOrderRefNumbers")
 void updateTransactionStatusToFailed(@Param("sbiOrderRefNumbers") List<String> sbiOrderRefNumbers);
 
 @Transactional
@@ -17,44 +16,3 @@ void updateTransactionStatusToFailed(@Param("sbiOrderRefNumbers") List<String> s
        "AND EXISTS (SELECT 1 FROM Transaction t WHERE t.sbiOrderRefNumber = o.sbiOrderRefNumber " +
        "AND t.transactionStatus = 'BOOKED' AND t.paymentStatus = 'PAYMENT_INITIATED_START')")
 void updateOrderStatusToFailed(@Param("sbiOrderRefNumbers") List<String> sbiOrderRefNumbers);
-
-
-
-@Transactional
-@Modifying
-@Query("UPDATE Orders o " +
-       "SET o.status = 'EXPIRED' " +
-       "WHERE o.status = 'ATTEMPTED' " +
-       "AND o.sbiOrderRefNumber IN :sbiOrderRefNumbers " +
-       "AND EXISTS (SELECT 1 FROM Transaction t WHERE t.sbiOrderRefNumber = o.sbiOrderRefNumber " +
-       "AND t.transactionStatus = 'BOOKED' AND t.paymentStatus = '')")
-void updateOrderStatusToExpired(@Param("sbiOrderRefNumbers") List<String> sbiOrderRefNumbers);
-
-@Transactional
-@Modifying
-@Query("UPDATE Orders o " +
-       "SET o.status = 'EXPIRED' " +
-       "WHERE o.status = 'CREATED' " +
-       "AND o.sbiOrderRefNumber IN :sbiOrderRefNumbers " +
-       "AND EXISTS (SELECT 1 FROM Transaction t WHERE t.sbiOrderRefNumber = o.sbiOrderRefNumber " +
-       "AND t.transactionStatus = '' AND t.paymentStatus = '')")
-void updateOrderStatusToExpired(@Param("sbiOrderRefNumbers") List<String> sbiOrderRefNumbers);
-
-
- @Modifying
-    @Query("UPDATE Transaction t " +
-            "SET t.transactionStatus = 'FAILED' " +
-            "WHERE t.transactionStatus = 'BOOKED' " +
-            "AND t.paymentStatus = 'PAYMENT_INITIATED_START' " +
-            "AND t.sbiOrderRefNumber IN :sbiOrderRefNumbers " +
-            "AND EXISTS (SELECT 1 FROM Order o WHERE o.sbiOrderRefNumber = t.sbiOrderRefNumber AND o.status = 'ATTEMPTED')")
-    void updateTransactionStatusToFailed(@Param("sbiOrderRefNumbers") List<String> sbiOrderRefNumbers);
-
-    @Modifying
-    @Query("UPDATE Order o " +
-            "SET o.status = 'FAILED' " +
-            "WHERE o.status = 'ATTEMPTED' " +
-            "AND o.sbiOrderRefNumber IN :sbiOrderRefNumbers " +
-            "AND EXISTS (SELECT 1 FROM Transaction t WHERE t.sbiOrderRefNumber = o.sbiOrderRefNumber " +
-            "AND t.transactionStatus = 'BOOKED' AND t.paymentStatus = 'PAYMENT_INITIATED_START')")
-    void updateOrderStatusToFailed(@Param("sbiOrderRefNumbers") List<String> sbiOrderRefNumbers);
