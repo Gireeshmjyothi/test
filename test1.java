b2185f8e907cd9bfd7d5870e19986e939c7b682b
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-01-22 14:45:30 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,3 +1,10 @@
 Error starting ApplicationContext. To display the condition evaluation report re-run your application with 'debug' enabled.
 2025-01-22 14:42:05 [main] ERROR o.s.boot.SpringApplication  - userId= - correlationId= - Application run failed
 org.springframework.beans.factory.NoUniqueBeanDefinitionException: No qualifying bean of type 'org.apache.geode.cache.client.ClientCache' available: expected single matching bean but found 2: gemfireCache,clientCacheFactoryBean
+
+  @Configuration
+@ClientCacheApplication
+@EnableEntityDefinedRegions(basePackages = ENTITY_DEFINED_REGIONS )
+@EnableGemfireRepositories(basePackages = CACHE_REPOSITORIES )
+public class GemFireConfiguration  {
+}
