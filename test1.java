aad0a1538237b28efd4e435b9cc744f658a86d2b
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-01-24 15:45:47 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,29 +1,4 @@
-Error starting ApplicationContext. To display the condition evaluation report re-run your application with 'debug' enabled.
-2025-01-22 14:42:05 [main] ERROR o.s.boot.SpringApplication  - userId= - correlationId= - Application run failed
-org.springframework.beans.factory.NoUniqueBeanDefinitionException: No qualifying bean of type 'org.apache.geode.cache.client.ClientCache' available: expected single matching bean but found 2: gemfireCache,clientCacheFactoryBean
-
-  @Configuration
-@ClientCacheApplication
-@EnableEntityDefinedRegions(basePackages = ENTITY_DEFINED_REGIONS )
-@EnableGemfireRepositories(basePackages = CACHE_REPOSITORIES )
-public class GemFireConfiguration  {
-}
-
-
-@Configuration
-public class GeodeConfig {
-    @Bean
-    public PdxSerializer pdxSerializer(){
-        return new ReflectionBasedAutoSerializer("com.epay.admin.*");
-    }
-
-    @Bean(name = "customClientCache")
-    public ClientCacheFactoryBean clientCacheFactoryBean(){
-        ClientCacheFactoryBean cacheFactoryBean = new ClientCacheFactoryBean();
-        cacheFactoryBean.setPdxSerializer(pdxSerializer());
-        cacheFactoryBean.setPdxReadSerialized(true);
-        return  cacheFactoryBean;
-    }
-}
-
-PDX[10247791,com.epay.admin.entity.cache.MerchantPayModeDownTimeCacheEntity]{id=76}
+implementation 'com.vmware.gemfire:gemfire-core:10.1.2'
+	implementation 'com.vmware.gemfire:spring-boot-3.3-gemfire-10.1:2.0.3'
+	implementation 'com.vmware.gemfire:gemfire-cq:10.1.2'
+  
