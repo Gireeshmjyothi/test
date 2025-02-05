3b73fb06b21b1462825d823ee4b45ef4f01017fe
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-05 13:16:29 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,1 +1,14 @@
-Caused by: org.hibernate.HibernateException: Unable to determine Dialect without JDBC metadata (please set 'jakarta.persistence.jdbc.url' for common cases or 'hibernate.dialect' when a custom Dialect implementation must be provided)
+Parameter 0 of constructor in com.sbi.epay.notification.thirdpartyservice.EmailClient required a bean of type 'org.springframework.mail.javamail.JavaMailSender' that could not be found.
+
+
+Action:
+
+Consider defining a bean of type 'org.springframework.mail.javamail.JavaMailSender' in your configuration.  
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
+@ComponentScan(basePackages = {"org.springframework.boot.context.embedded.tomcat", "com.epay.transaction", "com.sbi.epay"})
+@EnableJpaRepositories(basePackages = "com.epay.transaction")
+@EntityScan(basePackages = "com.epay.transaction.entity")
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableScheduling
+public class EpayTransactionServiceApplication implements WebMvcConfigurer {
