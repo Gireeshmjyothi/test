9344c7579f4dce21a95a1a228ccbe3a69595d64e
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-06 16:37:03 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,14 +1,40 @@
-Parameter 0 of constructor in com.sbi.epay.notification.thirdpartyservice.EmailClient required a bean of type 'org.springframework.mail.javamail.JavaMailSender' that could not be found.
+Description:
+
+Parameter 0 of constructor in com.epay.transaction.config.kafka.env.KafkaProducerConfig required a bean of type 'com.epay.transaction.config.kafka.env.KafkaProducerSettings' that could not be found.
 
 
 Action:
 
-Consider defining a bean of type 'org.springframework.mail.javamail.JavaMailSender' in your configuration.  
+Consider defining a bean of type 'com.epay.transaction.config.kafka.env.KafkaProducerSettings' in your configuration.
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
-@ComponentScan(basePackages = {"org.springframework.boot.context.embedded.tomcat", "com.epay.transaction", "com.sbi.epay"})
-@EnableJpaRepositories(basePackages = "com.epay.transaction")
-@EntityScan(basePackages = "com.epay.transaction.entity")
-@EnableJpaAuditing(auditorAwareRef = "auditorAware")
-@EnableScheduling
-public class EpayTransactionServiceApplication implements WebMvcConfigurer {
+
+@Configuration
+public class KafkaProducerConfig {
+    private final LoggerUtility log = LoggerFactoryUtility.getLogger(this.getClass());
+    private final KafkaProducerSettings kafkaProducerSettings;
+
+    public KafkaProducerConfig(KafkaProducerSettings kafkaProducerSettings) {
+        this.kafkaProducerSettings = kafkaProducerSettings;
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> stringKafkaTemplate() {
+        return new KafkaTemplate<>(stringProducerFactory());
+    }
+
+    @Bean
+    public KafkaTemplate<String, byte[]> byteArrayKafkaTemplate() {
+        return new KafkaTemplate<>(byteArrayProducerFactory());
+    }
+}
+
+
+@Getter
+@Setter
+@Component
+public abstract class KafkaProducerSettings {
+    @Value("${spring.kafka.bootstrapServers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.producer.acks}")
+}
