bd20b094182df8c46ca4333d7e05f552041e3271
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-07 11:43:37 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,40 +1,39 @@
-Description:
+<!DOCTYPE html>
 
-Parameter 0 of constructor in com.epay.transaction.config.kafka.env.KafkaProducerConfig required a bean of type 'com.epay.transaction.config.kafka.env.KafkaProducerSettings' that could not be found.
-
-
-Action:
-
-Consider defining a bean of type 'com.epay.transaction.config.kafka.env.KafkaProducerSettings' in your configuration.
-
-
-@Configuration
-public class KafkaProducerConfig {
-    private final LoggerUtility log = LoggerFactoryUtility.getLogger(this.getClass());
-    private final KafkaProducerSettings kafkaProducerSettings;
-
-    public KafkaProducerConfig(KafkaProducerSettings kafkaProducerSettings) {
-        this.kafkaProducerSettings = kafkaProducerSettings;
-    }
-
-    @Bean
-    public KafkaTemplate<String, String> stringKafkaTemplate() {
-        return new KafkaTemplate<>(stringProducerFactory());
-    }
-
-    @Bean
-    public KafkaTemplate<String, byte[]> byteArrayKafkaTemplate() {
-        return new KafkaTemplate<>(byteArrayProducerFactory());
-    }
-}
-
-
-@Getter
-@Setter
-@Component
-public abstract class KafkaProducerSettings {
-    @Value("${spring.kafka.bootstrapServers}")
-    private String bootstrapServers;
-
-    @Value("${spring.kafka.producer.acks}")
-}
+SBIePAY_ATRN Booked Customer
+<html>
+<head>
+    <style>
+        body {
+            font: 12px/18px "Trebuchet MS";
+        }
+    </style>
+</head>
+<body>
+<br/>
+Dear Customer,
+<br/></br>
+You have booked an order on {Merchant Name (Brand Name)} with the below details:
+<br/><br/>
+&#45;&nbsp;Merchant Website: {Merchant URL}
+<br/>
+&#45;&nbsp;Merchant Order Number: {Merchant Order ID}
+<br/>
+&#45;&nbsp;SBIePay transaction reference number: {ATRN}
+<br/>
+&#45;&nbsp;Transaction booking date: {Txn Date}
+<br/>
+&#45;&nbsp;Transaction amount: {Posting Amount}
+<br/>
+&#45;&nbsp;Transaction status: {Status_Description}
+<br/><br/>
+For any further assistance, please contact your merchant.
+<br/><br/>
+Regards
+<br/>
+SBIePay Team
+<br/><br/>
+<b>Note - This is an auto generated e mail and you need not reply to this.</b>
+</body>
+</html>
+</html>
