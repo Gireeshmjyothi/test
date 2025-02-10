4c417f6f8b222061ae5d377d26f517c685569a0a
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-10 12:34:29 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,1 +1,30 @@
-^(http:\/\/|https:\/\/)?([\w.-]+)\.([a-z]{2,})(\/[\w.-]*)*(\/#[\w-]*)?$
+ String requestBody = getSMSData(smsDto);
+        logger.info("SMS notification start executed for smsData {} ", requestBody);
+        try {
+            Map<String, String> errorDetails = new HashMap<>();
+            HttpStatusCode status = getWebClient().post().uri(smsBasePath + smsURL).header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE).header(HttpHeaders.AUTHORIZATION, "Basic " + createBasicAuthHeader(smsUserName, smsPassword)).bodyValue(requestBody).retrieve().onStatus(HttpStatusCode::isError, response -> response.bodyToMono(String.class).flatMap(errorBody -> {
+                logger.error("Error response of SMS notification is {} for smsData {} ", errorBody, smsDto);
+                Arrays.stream(errorBody.split("&")).map(pair -> pair.split("=")).forEach(pair -> errorDetails.put(pair[0], pair.length > 1 ? pair[1] : ""));
+                return Mono.error(new NotificationException(response.statusCode().toString(), "Error during request: " + errorDetails));
+            })).toBodilessEntity()
+                    .doOnSuccess(response -> {
+                        logger.info("SMS API response status: {}", response.getStatusCode());
+                        logger.info("SMS API response.body: {}", response.getBody());
+                        logger.info("SMS API full response: {}", response);
+                    })
+                    .doOnError(error -> logger.info("Error occurred while calling sms api: {}", error.getMessage(), error))
+                    .map(ResponseEntity::getStatusCode)
+                    .block(); // Blocking for simplicity in this example
+
+            assert status != null;
+            if (status.is2xxSuccessful()) {
+                logger.debug("Success status of SMS notification is {} for smsData {} ", status, smsDto);
+                return true;
+            } else {
+                logger.error("Error status of SMS notification is {} for smsData {} ", status, smsDto);
+                throw new NotificationException(NotificationConstant.FAILURE_CODE, MessageFormat.format(NotificationConstant.FAILURE_MSG, "SMS"));
+            }
+        } catch (Exception e) {
+            logger.error("Error in SMS notification {} for smsData {} ", e.getMessage(), smsDto);
+            throw new NotificationException(NotificationConstant.FAILURE_CODE, MessageFormat.format(NotificationConstant.FAILURE_MSG, "SMS"));
+        }
