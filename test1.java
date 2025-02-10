f0848c0f882dbe59b8bfdceefea54d038dd13394
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-10 17:06:15 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,7 +1,4 @@
- String requestBody = getSMSData(smsDto);
-        logger.info("SMS notification start executed for smsData {} ", requestBody);
-        try {
-            Map<String, String> errorDetails = new HashMap<>();
+Map<String, String> errorDetails = new HashMap<>();
             HttpStatusCode status = getWebClient().post().uri(smsBasePath + smsURL).header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE).header(HttpHeaders.AUTHORIZATION, "Basic " + createBasicAuthHeader(smsUserName, smsPassword)).bodyValue(requestBody).retrieve().onStatus(HttpStatusCode::isError, response -> response.bodyToMono(String.class).flatMap(errorBody -> {
                 logger.error("Error response of SMS notification is {} for smsData {} ", errorBody, smsDto);
                 Arrays.stream(errorBody.split("&")).map(pair -> pair.split("=")).forEach(pair -> errorDetails.put(pair[0], pair.length > 1 ? pair[1] : ""));
@@ -12,7 +9,11 @@
                         logger.info("SMS API response.body: {}", response.getBody());
                         logger.info("SMS API full response: {}", response);
                     })
-                    .doOnError(error -> logger.info("Error occurred while calling sms api: {}", error.getMessage(), error))
+                    .doOnError(error -> {
+                        logger.error("Error occurred while calling sms api : {}", error.getMessage());
+                        logger.error("Error Object : {0}", error);
+                        error.printStackTrace();
+                    })
                     .map(ResponseEntity::getStatusCode)
                     .block(); // Blocking for simplicity in this example
 
@@ -28,3 +29,7 @@
             logger.error("Error in SMS notification {} for smsData {} ", e.getMessage(), smsDto);
             throw new NotificationException(NotificationConstant.FAILURE_CODE, MessageFormat.format(NotificationConstant.FAILURE_MSG, "SMS"));
         }
+
+
+curl -k -u 'epaypgotpdom:Ep@y1Dddt' -X POST "https://example.test.co.in:4244/bmg/sms/epaypgotpdom" --data "content_type=text&sender_id=SBIBNK&mobile=8073012970&message=Transaction Success Notification Dear Customer, Transaction of INR 99.9 on NB at Cedge pvt ltd on 23/01/2025 has been successfully processed through SBIePay.&intflag=0&charging=0"
+error_code=10008&error_desc=MESSAGE_TEMPLATE_VERIFY_FAILED&umid=112502101548202571914311275
