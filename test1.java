ad25b1cb62de4558a8322739d394966cb2114ff5
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-14 00:32:22 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,45 +1,96 @@
+package com.epay.payment.service;
+
+import com.epay.payment.config.EmailSmsConfig;
+import com.epay.payment.dao.CustomerDao;
+import com.epay.payment.dao.NotificationDao;
+import com.epay.payment.dao.OrderDao;
+import com.epay.payment.dto.*;
+import com.epay.payment.etl.producer.EmailNotificationProducer;
+import com.epay.payment.etl.producer.SmsNotificationProducer;
+import com.epay.payment.exceptions.PaymentException;
+import com.epay.payment.externalservice.AdminServiceClient;
+import com.epay.payment.util.EmailUtil;
+import com.epay.payment.util.ErrorConstants;
+import com.epay.payment.util.SmsUtil;
+import com.epay.payment.util.enums.NotificationTemplateType;
+import com.epay.payment.util.enums.RequestType;
+import com.sbi.epay.logging.utility.LoggerFactoryUtility;
+import com.sbi.epay.logging.utility.LoggerUtility;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.text.MessageFormat;
+import java.util.*;
+
 @Service
-public class NotificationService {
+@RequiredArgsConstructor
+public class PaymentNotificationService {
+    private final LoggerUtility logger = LoggerFactoryUtility.getLogger(this.getClass());
+    private final NotificationDao notificationDao;
+    private final AdminServiceClient adminServiceClient;
+    private final EmailNotificationProducer emailNotificationProducer;
+    private final SmsNotificationProducer smsNotificationProducer;
+    private final EmailSmsConfig emailSmsConfig;
+    private final CustomerDao customerDao;
+    private final OrderDao orderDao;
 
-    private final Map<String, Consumer<String>> notificationActions;
-
-    public NotificationService() {
-        this.notificationActions = Map.of(
-            "smsMerchant", phone -> sendSms(phone, "Merchant SMS Notification"),
-            "smsCustomer", phone -> sendSms(phone, "Customer SMS Notification"),
-            "emailMerchant", email -> sendEmail(email, "Merchant Email Notification", "Your merchant email content here"),
-            "emailCustomer", email -> sendEmail(email, "Customer Email Notification", "Your customer email content here")
-        );
+    public <T> String publishPaymentNotification(T dto, Class<T> type, NotificationTemplateType notificationTemplateType, RequestType requestType) {
+        MerchantNotificationViewDto merchantNotificationViewDto;
+        CustomerDto customerDto;
+        
+        if (type.isAssignableFrom(OrderDto.class)) {
+            OrderDto orderDto = (OrderDto) dto;
+            merchantNotificationViewDto = adminServiceClient.getMerchantNotification(orderDto.getMId());
+            customerDto = customerDao.getCustomerByCustomerId(orderDto.getCustomerId());
+            sendNotification(merchantNotificationViewDto, customerDto, orderDto, notificationTemplateType, requestType);
+        } else if (type.isAssignableFrom(TransactionDto.class)) {
+            TransactionDto transactionDto = (TransactionDto) dto;
+            merchantNotificationViewDto = adminServiceClient.getMerchantNotification(transactionDto.getMerchantId());
+            customerDto = orderDao.getCustomerBySbiOrderRefNumber(transactionDto.getSbiOrderRefNumber());
+            sendNotification(merchantNotificationViewDto, customerDto, transactionDto, notificationTemplateType, requestType);
+        } else {
+            throw new PaymentException(ErrorConstants.NOT_FOUND_ERROR_CODE, 
+                MessageFormat.format(ErrorConstants.NOT_FOUND_ERROR_MESSAGE, "Invalid class type"));
+        }
+        return "Notification processed successfully.";
     }
 
-    public void processNotification(NotificationDTO notificationDTO, String merchantEmail, String merchantPhone, String customerEmail, String customerPhone) {
-        Map<String, String> recipients = Map.of(
-            "smsMerchant", merchantPhone,
-            "smsCustomer", customerPhone,
-            "emailMerchant", merchantEmail,
-            "emailCustomer", customerEmail
-        );
-
-        Map<String, String> flags = Map.of(
-            "smsMerchant", notificationDTO.getSmsForMerchant(),
-            "smsCustomer", notificationDTO.getSmsForCustomer(),
-            "emailMerchant", notificationDTO.getEmailForMerchant(),
-            "emailCustomer", notificationDTO.getEmailForCustomer()
-        );
-
-        // Use Streams to process notifications dynamically
-        flags.forEach((key, value) -> {
-            if ("Y".equalsIgnoreCase(value)) {
-                notificationActions.get(key).accept(recipients.get(key));
-            }
+    private <T> void sendNotification(MerchantNotificationViewDto merchantNotificationViewDto, CustomerDto customerDto, T object, NotificationTemplateType notificationTemplateType, RequestType requestType) {
+        Optional.ofNullable(merchantNotificationViewDto).ifPresent(merchant -> {
+            Optional.ofNullable(customerDto).ifPresent(customer -> {
+                if ("Y".equalsIgnoreCase(merchant.getEmailAlertCustomer())) {
+                    sendEmailNotification(merchant, customer, object, notificationTemplateType, requestType, customer.getEmail());
+                }
+                if ("Y".equalsIgnoreCase(merchant.getEmailAlertMerchant())) {
+                    sendEmailNotification(merchant, customer, object, notificationTemplateType, requestType, merchant.getCommunicationEmail());
+                }
+                if ("Y".equalsIgnoreCase(merchant.getSmsAlertCustomer())) {
+                    sendSmsNotification(object, notificationTemplateType, requestType, customer.getPhoneNumber());
+                }
+                if ("Y".equalsIgnoreCase(merchant.getSmsAlertMerchant())) {
+                    sendSmsNotification(object, notificationTemplateType, requestType, merchant.getMobileNo());
+                }
+            });
         });
     }
 
-    private void sendSms(String phoneNumber, String message) {
-        System.out.println("Sending SMS to " + phoneNumber + ": " + message);
+    private <T> void sendEmailNotification(MerchantNotificationViewDto merchant, CustomerDto customer, T object, NotificationTemplateType notificationTemplateType, RequestType requestType, String recipientEmail) {
+        PaymentEmailDto emailDto = PaymentEmailDtoFactory.create(merchant, object, notificationTemplateType, requestType, recipientEmail, emailSmsConfig);
+        emailNotificationProducer.publish(emailDto.getRequestType(), "TransactionCustomerEmail", emailDto);
     }
 
-    private void sendEmail(String email, String subject, String body) {
-        System.out.println("Sending Email to " + email + " - Subject: " + subject);
+    private <T> void sendSmsNotification(T object, NotificationTemplateType notificationTemplateType, RequestType requestType, String mobileNumber) {
+        PaymentSmsDto smsDto = PaymentSmsDtoFactory.create(object, notificationTemplateType, requestType, mobileNumber);
+        smsNotificationProducer.publish(smsDto.getRequestType(), "TransactionCustomerSms", smsDto);
+    }
+
+    public void sendEmail(PaymentEmailDto paymentEmailDto) {
+        logger.info("Sending Email Notification");
+        notificationDao.sendEmailNotification(paymentEmailDto);
+    }
+
+    public void sendSms(PaymentSmsDto paymentSmsDto) {
+        logger.info("Sending SMS Notification");
+        notificationDao.sendSmsNotification(paymentSmsDto);
     }
 }
