634b8ea0367a7a6ccc466bf4dcd5c35eb477be86
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-14 20:04:10 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,31 +1,3 @@
-public MerchantNotificationViewDto getMerchantNotification(String mId) {
-    logger.info("Preparing request to get merchant notification view for merchant ID: {}", mId);
-    
-    URI uri = URI.create(getBaseUrl() + MessageFormat.format(MERCHANT_NOTIFICATION_ENDPOINT, mId));
-
-    ResponseDto<MerchantNotificationViewDto> responseDto = getWebClient()
-            .post()
-            .uri(uri)
-            .retrieve()
-            .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
-                logger.error("Received 4xx error from Admin Service for merchant ID: {}", mId);
-                return Mono.error(new PaymentException(ErrorConstants.BAD_REQUEST_ERROR_CODE, 
-                    MessageFormat.format(ErrorConstants.BAD_REQUEST_ERROR_MESSAGE, "Admin Service")));
-            })
-            .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> {
-                logger.error("Received 5xx error from Admin Service for merchant ID: {}", mId);
-                return Mono.error(new PaymentException(ErrorConstants.EXTERNAL_SERVICE_ERROR_CODE, 
-                    MessageFormat.format(ErrorConstants.EXTERNAL_SERVICE_ERROR_MESSAGE, "Admin Service")));
-            })
-            .bodyToMono(new ParameterizedTypeReference<ResponseDto<MerchantNotificationViewDto>>() {})
-            .block();
-
-    if (responseDto == null || responseDto.getStatus().equals(PaymentConstants.FAILURE_RESPONSE_CODE) || responseDto.getData() == null) {
-        logger.error("Merchant Notification not found for merchant ID: {}", mId);
-        throw new PaymentException(ErrorConstants.NOT_FOUND_ERROR_CODE, 
-                MessageFormat.format(ErrorConstants.NOT_FOUND_ERROR_MESSAGE, "Merchant Notification"));
-    }
-
-    logger.info("Successfully retrieved Merchant Notification for merchant ID: {}", mId);
-    return responseDto.getData(); 
-}
+2025-02-14 14:15:30 [org.springframework.kafka.KafkaListenerEndpointContainer#0-0-C-1] ERROR org.thymeleaf.TemplateEngine  - userId= - correlationId= - [THYMELEAF][org.springframework.kafka.KafkaListenerEndpointContainer#0-0-C-1] Exception processing template "TRANSACTION_SUCCESS": An error happened during template parsing (template: "templates/TRANSACTION_SUCCESS.html")
+org.thymeleaf.exceptions.TemplateInputException: An error happened during template parsing (template: "templates/TRANSACTION_SUCCESS.html")
+	at org.thymeleaf.templateparser.markup.AbstractMarkupTemplateParser.parse(AbstractMarkupTemplateParser.java:235)
