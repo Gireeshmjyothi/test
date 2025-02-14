33174c8f57e8ccc5c9fe8e761ccd10ce1c4d7e47
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-14 20:12:59 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -27,4 +27,24 @@ has been successfully processed through SBIePay.
 public static Map<String, Object> generatedTransactionNotification(NotificationDto notificationDto) {
         return Map.of("currencyCode", notificationDto.getCurrencyCode(), "getGtwPostingAmount", notificationDto.getGtwPostingAmount(), "getPayMode", notificationDto.getPayMode(), "txnTime", notificationDto.getTxnDate());
     }
+<!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org">
+<head>
+    <style>
+        body {
+            font: 12px/18px "Trebuchet MS";
+        }
+    </style>
+</head>
+<body>
+    Transaction of <span th:text="${currencyCode} + ' ' + ${gtwPostingAmt}"></span>
+    on <span th:text="${paymode}"></span>
+    at <span th:text="${merchantBrandName}"></span>
+    on <span th:text="${txnDate}"></span>
+    has been successfully processed through SBIePay.
 
+    <br/><br/>
+    <b>Note - This is an auto-generated email, and you do not need to reply to this.</b>
+</body>
+</html>
+	    
