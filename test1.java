26256facb2846145f727cde3275deeea8407dcca
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-15 18:11:48 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -4,12 +4,13 @@
                 INNER JOIN Order o
                 ON t.orderRefNumber = o.orderRefNumber
                 WHERE t.atrnNumber = :atrnNumber
+                AND t.pushResponse = 'N'
                 AND (:orderRefNumber IS NULL OR t.orderRefNumber = :orderRefNumber)
                 AND (:sbiOrderRefNumber IS NULL OR t.sbiOrderRefNumber = :sbiOrderRefNumber)
                 AND (:orderAmount IS NULL OR o.orderAmount = :orderAmount)
             """)
-    Optional<List<Object[]>> fetchTransactionAndOrderDetail(
-            @Param("atrnNumber") String atrnNumber,
-            @Param("orderRefNumber") String orderRefNumber,
-            @Param("sbiOrderRefNumber") String sbiOrderRefNumber,
-            @Param("orderAmount") BigDecimal orderAmount);
+Optional<List<Object[]>> fetchTransactionAndOrderDetail(
+        @Param("atrnNumber") String atrnNumber,
+        @Param("orderRefNumber") String orderRefNumber,
+        @Param("sbiOrderRefNumber") String sbiOrderRefNumber,
+        @Param("orderAmount") BigDecimal orderAmount);
