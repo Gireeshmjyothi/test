c0e70453fb844fa24e597225637691621bd79d4b
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-17 17:41:55 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -4,6 +4,14 @@
                 INNER JOIN Order o
                 ON t.orderRefNumber = o.orderRefNumber
                 WHERE t.atrnNum = :atrnNumber
-                AND t.pushResponse LIKE %:pushStatus%
+                AND DBMS_LOB.SUBSTR(t.pushResponse, 4000) LIKE %:pushStatus%
             """)
-    Optional<List<Object[]>> fetchTransactionAndOrderDetail(@Param("atrnNumber") String atrnNumber, @Param("pushStatus") String pushStatus);
+Optional<List<Object[]>> fetchTransactionAndOrderDetail(@Param("atrnNumber") String atrnNumber, @Param("pushStatus") String pushStatus);@Query(value = """
+                SELECT t, o
+                FROM Transaction t
+                INNER JOIN Order o
+                ON t.orderRefNumber = o.orderRefNumber
+                WHERE t.atrnNum = :atrnNumber
+                AND DBMS_LOB.SUBSTR(t.pushResponse, 4000) LIKE %:pushStatus%
+            """)
+Optional<List<Object[]>> fetchTransactionAndOrderDetail(@Param("atrnNumber") String atrnNumber, @Param("pushStatus") String pushStatus);
