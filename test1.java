5bfe17debd31bd8537c996275f92c99af7f56576
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-17 12:33:44 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,14 +1,29 @@
-spring.kafka.consumer.groupId=payment-consumers
-spring.kafka.consumer.enableAutoCommit=true
-spring.kafka.consumer.autoCommitInterval=100
-spring.kafka.consumer.sessionTimeoutMS=300000
-spring.kafka.consumer.requestTimeoutMS=420000
-spring.kafka.consumer.fetchMaxWaitMS=200
-spring.kafka.consumer.maxPollRecords=5
-spring.kafka.consumer.autoOffsetReset=latest
-spring.kafka.consumer.keyDeserializer=org.apache.kafka.common.serialization.StringDeserializer
-spring.kafka.consumer.valueDeserializer=org.apache.kafka.common.serialization.StringDeserializer
-spring.kafka.consumer.retryMaxAttempts=3
-spring.kafka.consumer.retryBackOffInitialIntervalMS=10000
-spring.kafka.consumer.retryBackOffMaxIntervalMS=30000
-spring.kafka.consumer.numberOfConsumers=1
+@Component
+@RequiredArgsConstructor
+public class PaymentPushVerificationListener {
+    private final LoggerUtility log = LoggerFactoryUtility.getLogger(this.getClass());
+    private final ObjectMapper objectMapper;
+    private final PaymentPushVerificationService paymentPushVerificationService;
+    private final KafkaTemplate kafkaTemplate;
+
+    @KafkaListener(topics = "${spring.kafka.topic.payment.push.verification}")
+    public void paymentPushVerification(ConsumerRecord<String, String> consumerRecord, Acknowledgment acknowledgment) {
+        log.debug("Push verification request received for key : {} and value : {}", consumerRecord.key(), consumerRecord.value());
+        String message = consumerRecord.value();
+        try {
+            PaymentPushVerificationDto paymentPushVerificationDto = objectMapper.readValue(consumerRecord.value(), PaymentPushVerificationDto.class);
+            boolean processFlag = paymentPushVerificationService.processPaymentPushVerification(paymentPushVerificationDto);
+            if (processFlag) {
+                acknowledgment.acknowledge();
+            } else {
+                kafkaTemplate.send("retry-topic", message);
+            }
+        } catch (PaymentException e) {
+            log.error("PaymentException during onSmsMessage kafka listening message[key:{} and value: {}], error: {}", consumerRecord.key(), consumerRecord.value(), e.getErrorMessage());
+        } catch (JsonProcessingException e) {
+            log.error("JsonProcessingException during onSmsMessage kafka listening message[key:{} and value: {}], error: {}", consumerRecord.key(), consumerRecord.value(), e.getMessage());
+        } catch (Exception e) {
+            kafkaTemplate.send("dead-letter-topic", message);
+        }
+    }
+}
