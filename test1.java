f3abe8b11ed1a86b5bb00ed93081e69dd8a97a7d
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-17 17:39:29 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,9 +1,9 @@
-@Bean
-public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
-        ConsumerFactory<String, String> consumerFactory) {
-    ConcurrentKafkaListenerContainerFactory<String, String> factory =
-            new ConcurrentKafkaListenerContainerFactory<>();
-    factory.setConsumerFactory(consumerFactory);
-    factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
-    return factory;
-}
+@Query(value = """
+                SELECT t, o
+                FROM Transaction t
+                INNER JOIN Order o
+                ON t.orderRefNumber = o.orderRefNumber
+                WHERE t.atrnNum = :atrnNumber
+                AND t.pushResponse LIKE %:pushStatus%
+            """)
+    Optional<List<Object[]>> fetchTransactionAndOrderDetail(@Param("atrnNumber") String atrnNumber, @Param("pushStatus") String pushStatus);
