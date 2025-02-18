c36ee8859ca20de78533360e90e7e8ce450970ef
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-18 12:22:47 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,35 +1,26 @@
-@Query(value = """
-                SELECT t.*, o.*
-                FROM transaction t
-                INNER JOIN order o
-                ON t.order_ref_number = o.order_ref_number
-                WHERE t.atrn_num = :atrnNumber
-                AND DBMS_LOB.SUBSTR(t.push_response, 4000) LIKE %:pushStatus%
-            """, nativeQuery = true)
-Optional<List<Object[]>> fetchTransactionAndOrderDetail(@Param("atrnNumber") String atrnNumber, @Param("pushStatus") String pushStatus);
+@KafkaListener(topics = "${spring.kafka.topic.payment.push.verification}")
+    public void paymentPushVerification(ConsumerRecord<String, String> consumerRecord, Acknowledgment acknowledgment) {
+        log.debug("Push verification request received for key: {} and value: {}", consumerRecord.key(), consumerRecord.value());
+        String message = consumerRecord.value();
+        try {
+            PaymentPushVerificationDto paymentPushVerificationDto = objectMapper.readValue(message, PaymentPushVerificationDto.class);
+            boolean processFlag = paymentPushVerificationService.processPaymentPushVerification(paymentPushVerificationDto);
+            if (processFlag) {
+                acknowledgment.acknowledge();
+                log.info("Payment push notification message successfully processed and acknowledged for key: {}", consumerRecord.key());
+            } else {
+                kafkaTemplate.send("retry-topic", message);
+                log.warn("Processing failed, message sent to retry-topic for key: {}", consumerRecord.key());
+            }
+        } catch (JsonProcessingException e) {
+            log.error("JSON Parsing failed for key: {}, moving message to dead-letter-topic: {}", consumerRecord.key(), e.getMessage());
+            kafkaTemplate.send("dead-letter-topic", message);
 
-@Modifying
-@Query(value = """
-                UPDATE Transaction t
-                SET t.pushResponse = :pushResponse
-                WHERE t.atrnNum = :atrnNum
-            """, nativeQuery = true)
-int updatePushResponse(@Param("atrnNum") String atrnNum, @Param("pushResponse") String pushResponse);
+        } catch (PaymentException e) {
+            log.error("PaymentException occurred for key: {}, error: {}", consumerRecord.key(), e.getErrorMessage());
 
-@Query("SELECT t, o FROM Transaction t " +
-       "INNER JOIN Order o ON t.orderRefNumber = o.orderRefNumber " +
-       "WHERE t.atrnNum = :atrnNumber " +
-       "AND t.pushResponse LIKE CONCAT('%', :pushStatus, '%')")
-List<Object[]> fetchTransactionAndOrderDetail(@Param("atrnNumber") String atrnNumber, 
-                                               @Param("pushStatus") String pushStatus);
-
-
-@Modifying
- @Query(value = """
-        UPDATE Transaction t
-        SET t.pushResponse = :pushStatus
-        WHERE t.atrnNum = :atrnNumber
-    """)
- int updatePushResponseStatus(
-         @Param("atrnNumber") String atrnNumber,
-         @Param("pushStatus") String pushStatus);
+        } catch (Exception e) {
+            log.error("Unexpected error for key: {}, moving message to dead-letter-topic", consumerRecord.key(), e);
+            kafkaTemplate.send("dead-letter-topic", message);
+        }
+    }
