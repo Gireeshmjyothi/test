8ae375434f93b84e34a039350e968000254d676c
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-20 10:55:23 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,48 +1,25 @@
-@Configuration
-public class KafkaConsumerConfig {
-    private final KafkaConsumerSettings kafkaConsumerSettings;
+private static final LoggerUtility logger = LoggerFactoryUtility.getLogger(EncryptionDecryptionUtil.class);
 
-    public KafkaConsumerConfig(KafkaConsumerSettings kafkaConsumerSettings) {
-        this.kafkaConsumerSettings = kafkaConsumerSettings;
+    public String decryptRequest(String request, EncryptionKeyDto encryptionKeyDto) {
+        logger.info("Decryption request initiated...");
+
+        String encryptedKEK = DecryptionService.decryptValueBySecretKey(decodedValue(encryptionKeyDto.getAek()), encryptionKeyDto.getKek(), EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
+        logger.debug("kek got encrypted: {}", encryptedKEK);
+        String mek = DecryptionService.decryptValueBySecretKey(decodedValue(encryptedKEK), encryptionKeyDto.getMek(), EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
+        logger.debug("mek got decrypted");
+
+        logger.debug("request object decryption started {}", request);
+        return DecryptionService.decryptValueBySecretKey(decodedValue(mek), request, EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
     }
 
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> stringKafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(stringConsumerFactory());
-        factory.setConcurrency(kafkaConsumerSettings.getNumberOfConsumers());
-        return factory;
-    }
+    public String encryptRequest(String request, EncryptionKeyDto encryptionKeyDto) {
+        logger.info("Encryption request initiated...");
 
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, byte[]> byteArrayKafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, byte[]> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(byteArrayConsumerFactory());
-        factory.setConcurrency(kafkaConsumerSettings.getNumberOfConsumers());
-        return factory;
-    }
+        String encryptedKEK = DecryptionService.decryptValueBySecretKey(decodedValue(encryptionKeyDto.getAek()), encryptionKeyDto.getKek(), EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
+        logger.debug("kek got encrypted: {}", encryptedKEK);
+        String mek = DecryptionService.decryptValueBySecretKey(decodedValue(encryptedKEK), encryptionKeyDto.getMek(), EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
+        logger.debug("mek got decrypted");
 
-    private ConsumerFactory<String, String> stringConsumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs(StringDeserializer.class));
+        logger.debug("request object encryption started {}", request);
+        return EncryptionService.encryptValueBySecretKey(decodedValue(mek), request, EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
     }
-
-    private ConsumerFactory<String, byte[]> byteArrayConsumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs(ByteArrayDeserializer.class));
-    }
-
-    private Map<String, Object> consumerConfigs(Class<?> valueDeserializerClass) {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConsumerSettings.getBootstrapServers());
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaConsumerSettings.getGroupId());
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, kafkaConsumerSettings.isAutoCommitCursor());
-        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, kafkaConsumerSettings.getAutoCommitCursorIntervalMS());
-        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, kafkaConsumerSettings.getSessionTimeoutMS());
-        props.put(ConsumerConfig.REQUEST_TIMEOUT_MS_CONFIG, kafkaConsumerSettings.getRequestTimeoutMS());
-        props.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, kafkaConsumerSettings.getFetchMaxWaitMS());
-        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, kafkaConsumerSettings.getMaxPollRecords());
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, kafkaConsumerSettings.getOffsetReset());
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, kafkaConsumerSettings.getKeyDeserializer());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueDeserializerClass);
-        return props;
-    }
-}
