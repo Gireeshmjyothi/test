1910d882b85efce6b971280eeb1ad619f2794135
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-20 11:00:09 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,25 +1,40 @@
-private static final LoggerUtility logger = LoggerFactoryUtility.getLogger(EncryptionDecryptionUtil.class);
+public class EncryptionDecryptionUtil {
+    private static final LoggerUtility logger = LoggerFactoryUtility.getLogger(EncryptionDecryptionUtil.class);
 
     public String decryptRequest(String request, EncryptionKeyDto encryptionKeyDto) {
         logger.info("Decryption request initiated...");
+        String mek = getDecryptedMEK(encryptionKeyDto);
 
-        String encryptedKEK = DecryptionService.decryptValueBySecretKey(decodedValue(encryptionKeyDto.getAek()), encryptionKeyDto.getKek(), EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
-        logger.debug("kek got encrypted: {}", encryptedKEK);
-        String mek = DecryptionService.decryptValueBySecretKey(decodedValue(encryptedKEK), encryptionKeyDto.getMek(), EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
-        logger.debug("mek got decrypted");
-
-        logger.debug("request object decryption started {}", request);
+        logger.debug("Request object decryption started: {}", request);
         return DecryptionService.decryptValueBySecretKey(decodedValue(mek), request, EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
     }
 
     public String encryptRequest(String request, EncryptionKeyDto encryptionKeyDto) {
         logger.info("Encryption request initiated...");
+        String mek = getDecryptedMEK(encryptionKeyDto);
 
-        String encryptedKEK = DecryptionService.decryptValueBySecretKey(decodedValue(encryptionKeyDto.getAek()), encryptionKeyDto.getKek(), EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
-        logger.debug("kek got encrypted: {}", encryptedKEK);
-        String mek = DecryptionService.decryptValueBySecretKey(decodedValue(encryptedKEK), encryptionKeyDto.getMek(), EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
-        logger.debug("mek got decrypted");
-
-        logger.debug("request object encryption started {}", request);
+        logger.debug("Request object encryption started: {}", request);
         return EncryptionService.encryptValueBySecretKey(decodedValue(mek), request, EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, GCMIvLength.MAXIMUM, GCMTagLength.STANDARD);
     }
+
+    private String getDecryptedMEK(EncryptionKeyDto encryptionKeyDto) {
+        String encryptedKEK = DecryptionService.decryptValueBySecretKey(
+            decodedValue(encryptionKeyDto.getAek()), 
+            encryptionKeyDto.getKek(), 
+            EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, 
+            GCMIvLength.MAXIMUM, 
+            GCMTagLength.STANDARD
+        );
+        logger.debug("KEK decrypted successfully.");
+
+        String mek = DecryptionService.decryptValueBySecretKey(
+            decodedValue(encryptedKEK), 
+            encryptionKeyDto.getMek(), 
+            EncryptionDecryptionAlgo.AES_GCM_NO_PADDING, 
+            GCMIvLength.MAXIMUM, 
+            GCMTagLength.STANDARD
+        );
+        logger.debug("MEK decrypted successfully.");
+        return mek;
+    }
+}
