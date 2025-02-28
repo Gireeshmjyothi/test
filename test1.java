bf0737e0182e54e00df1ff5860cb8efc0dc1f17f
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-28 15:02:08 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -36,3 +36,26 @@ SBIePay Team
 </body>
 </html>
 </html>
+
+
+
+    <!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org">
+<head>
+    <style>
+        body {
+            font: 12px/18px "Trebuchet MS";
+        }
+    </style>
+</head>
+<body>
+Transaction of <span th:text="${currencyCode} + ' ' + ${gtwPostingAmt}"></span>
+on <span th:text="${payMode}"></span>
+at <span th:text="${merchantBrandName}"></span>
+on <span th:text="${txnDate}"></span>
+has been successfully processed through SBIePay.
+
+<br/><br/>
+<b>Note - This is an auto-generated email, and you do not need to reply to this.</b>
+</body>
+</html>
