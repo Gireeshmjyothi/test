818237da8e36a53a073cbe0362123cf9bc0a4608
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-02-28 15:01:45 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,3 +1,38 @@
-Map<String, Object> map = objectMapper.readValue(message, new TypeReference<Map<String, Object>>() {});
-String artnNumber = (String) map.getOrDefault("atrnNumber", "");
-String mId = (String) map.getOrDefault("merchantId", "");
+<!DOCTYPE html>
+SBIePAY_ATRN Booked Customer
+<html>
+<head>
+    <style>
+        body {
+            font: 12px/18px "Trebuchet MS";
+        }
+    </style>
+</head>
+<body>
+<br/>
+Dear Customer,
+<br/></br>
+You have booked an order on {Merchant Name (Brand Name)} with the below details:
+<br/><br/>
+&#45;&nbsp;Merchant Website: {Merchant URL}
+<br/>
+&#45;&nbsp;Merchant Order Number: {Merchant Order ID}
+<br/>
+&#45;&nbsp;SBIePay transaction reference number: {ATRN}
+<br/>
+&#45;&nbsp;Transaction booking date: {Txn Date}
+<br/>
+&#45;&nbsp;Transaction amount: {Posting Amount}
+<br/>
+&#45;&nbsp;Transaction status: {Status_Description}
+<br/><br/>
+For any further assistance, please contact your merchant.
+<br/><br/>
+Regards
+<br/>
+SBIePay Team
+<br/><br/>
+<b>Note - This is an auto generated email and you need not reply to this.</b>
+</body>
+</html>
+</html>
