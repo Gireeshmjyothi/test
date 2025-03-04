0ec540aadbf60d439bd7780721f7ce5c5644bdc1
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-03-04 12:17:03 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -13,3 +13,8 @@ COMMIT;
 @Query("SELECT mopDtl FROM MerchantOrderPaymentEntity mopDtl WHERE mopDtl.transactionStatus IN ('BOOKED', 'PENDING') " +
             "AND mopDtl.paymentStatus IN ('PAYMENT_INITIATION_START', 'PENDING')")
     List<MerchantOrderPaymentEntity> findMerchantOrderPaymentDetails();
+
+@Query("SELECT mopDtl FROM MerchantOrderPaymentEntity mopDtl WHERE " +
+       "(mopDtl.transactionStatus IN ('BOOKED', 'PENDING') AND mopDtl.paymentStatus IN ('PAYMENT_INITIATION_START', 'PENDING')) " +
+       "AND mopDtl.poolingStatus = 'P'")
+
