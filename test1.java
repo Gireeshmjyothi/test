4f7054137dd7ae52234ef0ae69059a25746e1577
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-03-07 15:13:52 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,10 +1,15 @@
-#kafka producer local setting
-spring.kafka.properties.security.protocol=SSL
-spring.kafka.properties.ssl.truststore.location=C:/certs/kafka/dev-cluster-cluster-ca-cert.p12
-spring.kafka.properties.ssl.truststore.password=Xe8FrxOGvVAx
-spring.kafka.properties.ssl.truststore.type=PKCS12
-spring.kafka.properties.ssl.keystore.location=C:/certs/kafka/dev-cluster-clients-ca-cert.p12
-spring.kafka.properties.ssl.keystore.password=J55FITkgEFid
-spring.kafka.properties.ssl.keystore.type=PKCS12
-
-spring.kafka.consumer.number-of-consumer=1
+spring:
+  kafka:
+    properties:
+      security.protocol: S
+      ssl:
+        truststore:
+          location: C:/certs/kafka/dev-cluster-cluster-ca-cert
+          password: Xe8FrxOG
+          type: PKCS
+        keystore:
+          location: C:/certs/kafka/dev-cluster-clients-ca-cert
+          password: J55Fooopd
+          type: P
+    consumer:
+      number-of-consumer: 1
