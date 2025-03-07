41682e59a76b7f04d7eda3ec14dd124854830932
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-03-07 14:47:17 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -44,3 +44,37 @@ public class KafkaProducerSettings {
     @Value("${spring.kafka.properties.ssl.keystore.type:}")
     private String keyType;
 }
+
+
+
+#Kafka consumer setting
+spring.kafka.consumer.groupId=transaction-consumers
+spring.kafka.consumer.enableAutoCommit=true
+spring.kafka.consumer.autoCommitInterval=100
+spring.kafka.consumer.sessionTimeoutMS=300000
+spring.kafka.consumer.requestTimeoutMS=420000
+spring.kafka.consumer.fetchMaxWaitMS=200
+spring.kafka.consumer.maxPollRecords=5
+spring.kafka.consumer.autoOffsetReset=latest
+spring.kafka.consumer.keyDeserializer=org.apache.kafka.common.serialization.StringDeserializer
+spring.kafka.consumer.valueDeserializer=org.apache.kafka.common.serialization.StringDeserializer
+spring.kafka.consumer.retryMaxAttempts=3
+spring.kafka.consumer.retryBackOffInitialIntervalMS=10000
+spring.kafka.consumer.retryBackOffMaxIntervalMS=30000
+spring.kafka.consumer.numberOfConsumers=1
+
+
+#Kafka producer setting
+spring.kafka.producer.acks=all
+spring.kafka.producer.retries=3
+spring.kafka.producer.batchSize=1000
+spring.kafka.producer.lingerMs=1
+spring.kafka.producer.bufferMemory=33554432
+spring.kafka.producer.keyDeserializer=org.apache.kafka.common.serialization.StringSerializer
+spring.kafka.producer.valueDeserializer=org.apache.kafka.common.serialization.StringSerializer
+
+#Topics
+spring.kafka.topic.transaction.notification.sms=transaction_sms_notification_topic
+spring.kafka.topic.transaction.notification.email=transaction_email_notification_topic
+spring.kafka.topic.partitions=4
+spring.kafka.topic.replicationFactor=1
