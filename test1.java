0c14064fb7de4bb88ac20f908410a90b43442be9
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-03-25 17:47:12 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,1 +1,1 @@
-This Gauge has been already registered (MeterId{name='kafka.consumer.coordinator.partition.lost.latency.max', tags=[tag(client.id=consumer-gatewayPooling-consumers-1),tag(kafka.version=3.7.2),tag(spring.id=kafkaConsumerFactory.consumer-gatewayPooling-consumers-1)]}), the Gauge registration will be ignored. Note that subsequent logs will be logged at debug level.
+ Statistics sampling thread detected a wakeup delay of 9665 ms, indicating a possible resource issue. Check the GC, memory, and CPU statistics
