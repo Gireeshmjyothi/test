10e925f4777d89c66a3b764ca8088731fcaf427b
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-04-02 18:00:20 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,4 +1,15 @@
- @Override
+  @Value("${security.cors.allowed.origins:*}")
+    private String allowedOrigins;
+
+    /**
+     * @param servletRequest  The request to process
+     * @param servletResponse The response associated with the request
+     * @param filterChain     Provides access to the next filter in the chain for this filter to pass the request and response
+     *                        to for further processing
+     * @throws IOException      Exception
+     * @throws ServletException Exception
+     */
+    @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         logger.info("Checking origin header");
         HttpServletRequest request = (HttpServletRequest) servletRequest;
@@ -12,4 +23,19 @@
         }
 
         filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    /**
+     * Create build error response string
+     * @return response string
+     */
+    private String buildErrorResponse() {
+        try {
+            ErrorDto errorDto = ErrorDto.builder().errorCode(ErrorConstants.CORS_ERROR_CODE).errorMessage(ErrorConstants.CORS_ERROR_MESSAGE).build();
+            AdminResponse<String> adminResponse = AdminResponse.<String>builder().status(AdminConstants.FAILURE_RESPONSE_CODE).errors(List.of(errorDto)).build();
+            return objectMapper.writeValueAsString(adminResponse);
+        } catch (JsonProcessingException e) {
+            logger.error("Json format error while generating error message");
+            return StringUtils.EMPTY;
+        }
     }
