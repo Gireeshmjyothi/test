283beaac44953182cd26d24ac33ef83605a9b33d
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-04-23 15:27:11 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,7 +1,11 @@
-  Properties connectionProperties = new Properties();
-           connectionProperties.put("user", user);
-           connectionProperties.put("password", password);
-           connectionProperties.put("driver", "oracle.jdbc.OracleDriver");
-
-
-java.lang.RuntimeException: oracle.jdbc.OracleDriver
+Dataset<Row> rowDataset = sparkSession.read()
+                   .format("jdbc")
+                   .option("url", url)
+                   .option("user", user)
+                   .option("password", password)
+                   .option("dbtable", table)
+                   .option("driver", "oracle.jdbc.OracleDriver")
+                   .load();
+       /* Dataset<Row> datasetRow = sparkSession.read().option("header", true).option("inferSchema", true)
+                .csv("data/employeeDb.csv").cache();*/
+           return rowDataset.as(merchantOrderPayment);
