0660165260dbebaa9323bf4636face5ca6d21529
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-04-23 17:00:23 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,16 +1,24 @@
- [UNRESOLVED_COLUMN.WITH_SUGGESTION] A column or function parameter with name `atrnNum` cannot be resolved. Did you mean one of the following? [`CIN`, `GST_IN`, `ATRN_NUM`, `PAY_MODE`, `DEBIT_AMT`]. 
-
+16:58:07.656 [http-nio-8080-exec-1] INFO  com.rajput.service.SparkService - Error while connect to SQL : [UNRESOLVED_COLUMN.WITH_SUGGESTION] A column or function parameter with name `sbiOrderRefNumber` cannot be resolved. Did you mean one of the following? [`ATRN_NUM`, `ORDER_AMOUNT`, `CHANNEL_BANK`, `CIN`, `FAIL_REASON`]. 
+16:58:07.659 [http-nio-8080-exec-1] ERROR org.apache.catalina.core.ContainerBase.[Tomcat].[localhost].[/].[dispatcherServlet] - Servlet.service() for servlet [dispatcherServlet] in context with path [] threw exception [Request processing failed: java.lang.RuntimeException: [UNRESOLVED_COLUMN.WITH_SUGGESTION] A column or function parameter with name `sbiOrderRefNumber` cannot be resolved. Did you mean one of the following? [`ATRN_NUM`, `ORDER_AMOUNT`, `CHANNEL_BANK`, `CIN`, `FAIL_REASON`].] with root cause
+java.lang.RuntimeException: [UNRESOLVED_COLUMN.WITH_SUGGESTION] A column or function parameter with name `sbiOrderRefNumber` cannot be resolved. Did you mean one of the following? [`ATRN_NUM`, `ORDER_AMOUNT`, `CHANNEL_BANK`, `CIN`, `FAIL_REASON`].
+	at com.rajput.service.SparkService.getDbDataSet(SparkService.java:97) ~[main/:?]
+	at com.rajput.service.SparkService.process(SparkService.java:29) ~[main/:?]
+	at com.rajput.controller.SparkController.home(SparkController.java:19) ~[main/:?]
 
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MerchantOrderPayment implements Serializable {
+    @JsonProperty("MERCHANT_ID")
     private String mid;
+    @JsonProperty("ORDER_REF_NUMBER")
     private String orderRefNumber;
     @JsonProperty("SBI_ORDER_REF_NUMBER")
     private String sbiOrderRefNumber;
+    @JsonProperty("ATRN_NUMBER")
     private String atrnNum;
+    @JsonProperty("DEBIT_AMT")
     private double debitAmt;
 }
 
@@ -26,19 +34,27 @@ CREATE TABLE "MERCHANT_ORDER_PAYMENTS" (
 	"GTW_MAP_ID" VARCHAR2(20 BYTE), 
 	"PAY_PROC_ID" VARCHAR2(20 BYTE))
 
+    Dataset<Row> datasetRow = sparkSession.read().option("header", true).option("inferSchema", true)
+                   .jdbc(url, table, connectionProperties);
 
-	 Dataset<Row> rowDataset = sparkSession.read()
+           /*Dataset<Row> rowDataset = sparkSession.read()
                    .format("jdbc")
                    .option("url", url)
                    .option("user", user)
                    .option("password", password)
                    .option("dbtable", table)
                    .option("driver", "oracle.jdbc.OracleDriver")
-                   .load();
+                   .load();*/
        /* Dataset<Row> datasetRow = sparkSession.read().option("header", true).option("inferSchema", true)
                 .csv("data/employeeDb.csv").cache();*/
-           return rowDataset.as(merchantOrderPayment);
+           datasetRow.withColumnRenamed("MERCHANT_ID", "mid");
+           datasetRow.withColumnRenamed("ORDER_REF_NUMBER", "orderRefNumber");
+           datasetRow.withColumnRenamed("SBI_ORDER_REF_NUMBER", "sbiOrderRefNumber");
+           datasetRow.withColumnRenamed("ATRN_NUM", "atrnNum");
+           datasetRow.withColumnRenamed("DEBIT_AMT", "debitAmt");
+           return datasetRow.as(merchantOrderPayment);
        }catch (Exception ex){
            log.info("Error while connect to SQL : {} ", ex.getMessage());
            throw new RuntimeException(ex.getMessage());
        }
+    }
