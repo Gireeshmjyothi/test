6c49df6049ae479dc3bab268153a25d3afb75a66
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-04-23 15:56:37 +0530

    Update error.java

--- error.java
+++ error.java
@@ -25,3 +25,20 @@ CREATE TABLE "MERCHANT_ORDER_PAYMENTS" (
 	"PAY_MODE" VARCHAR2(50 BYTE),
 	"GTW_MAP_ID" VARCHAR2(20 BYTE), 
 	"PAY_PROC_ID" VARCHAR2(20 BYTE))
+
+
+	 Dataset<Row> rowDataset = sparkSession.read()
+                   .format("jdbc")
+                   .option("url", url)
+                   .option("user", user)
+                   .option("password", password)
+                   .option("dbtable", table)
+                   .option("driver", "oracle.jdbc.OracleDriver")
+                   .load();
+       /* Dataset<Row> datasetRow = sparkSession.read().option("header", true).option("inferSchema", true)
+                .csv("data/employeeDb.csv").cache();*/
+           return rowDataset.as(merchantOrderPayment);
+       }catch (Exception ex){
+           log.info("Error while connect to SQL : {} ", ex.getMessage());
+           throw new RuntimeException(ex.getMessage());
+       }
