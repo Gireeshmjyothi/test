715d1ef8f1a313b0d25c049c293501d1d048d593
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-04-24 15:17:26 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,30 +1,12 @@
- private Dataset<MerchantOrderPayment> getDbDataSet() {
+public class QueryConstants {
 
-        try {
-            String url = "jdbc:oracle:thin:@10.177.134.124:1590:epaydbdev1";
-            String user = "GIREESH_M";
-            String password = "GIREESH_M";
-            String table = "MERCHANT_ORDER_PAYMENTS";
-            Properties connectionProperties = new Properties();
-            connectionProperties.put("user", user);
-            connectionProperties.put("password", password);
-            connectionProperties.put("driver", "oracle.jdbc.OracleDriver");
+    public static final String SELECT_LAST_24_HOURS_TEMPLATE = """
+        (SELECT * FROM %s
+         WHERE CREATED_DATE BETWEEN %d AND %d) AS TMP
+        """;
 
-            Dataset<Row> datasetRow = sparkSession.read().option("header", true).option("inferSchema", true)
-                    .jdbc(url, table, connectionProperties);
-
-            /* Dataset<Row> datasetRow = sparkSession.read().option("header", true).option("inferSchema", true)
-                .csv("data/employeeDb.csv").cache();*/
-            datasetRow.printSchema();
-            Dataset<Row> dataset = datasetRow.withColumnRenamed("MERCHANT_ID", "mid")
-                    .withColumnRenamed("ORDER_REF_NUMBER", "orderRefNumber")
-                    .withColumnRenamed("SBI_ORDER_REF_NUMBER", "sbiOrderRefNumber")
-                    .withColumnRenamed("ATRN_NUM", "atrnNum")
-                    .withColumnRenamed("DEBIT_AMT", "debitAmt");
-            dataset.printSchema();
-            return dataset.as(merchantOrderPayment);
-        } catch (Exception ex) {
-            log.info("Error while connect to SQL : {} ", ex.getMessage());
-            throw new RuntimeException(ex.getMessage());
-        }
+    public static String getLast24HoursQuery(String table, long currentMillis) {
+        long oneDayBefore = currentMillis - 24 * 60 * 60 * 1000;
+        return String.format(SELECT_LAST_24_HOURS_TEMPLATE, table, oneDayBefore, currentMillis);
     }
+}
