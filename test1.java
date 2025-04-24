907b09f526bd1105e5c4324f1e6b660c0edfb6b0
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-04-24 14:13:58 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,10 +1,30 @@
-responseDto.setMatchedRecords(matchedWithDB.collectAsList());
-responseDto.setFileRecordCount(datasetRowFile.count());
-responseDto.setUnMatchedRecords(
-    missMatchedWithDB.collectAsList().stream().map(fileRow -> {
-        UnMatchedDto unMatchedDto = new UnMatchedDto();
-        unMatchedDto.setDbMerchantOrderPayment(fileRow); // even though it's unmatched, you're storing file data
-        unMatchedDto.setNew(true);
-        return unMatchedDto;
-    }).collect(Collectors.toList())
-);
+ private Dataset<MerchantOrderPayment> getDbDataSet() {
+
+        try {
+            String url = "jdbc:oracle:thin:@10.177.134.124:1590:epaydbdev1";
+            String user = "GIREESH_M";
+            String password = "GIREESH_M";
+            String table = "MERCHANT_ORDER_PAYMENTS";
+            Properties connectionProperties = new Properties();
+            connectionProperties.put("user", user);
+            connectionProperties.put("password", password);
+            connectionProperties.put("driver", "oracle.jdbc.OracleDriver");
+
+            Dataset<Row> datasetRow = sparkSession.read().option("header", true).option("inferSchema", true)
+                    .jdbc(url, table, connectionProperties);
+
+            /* Dataset<Row> datasetRow = sparkSession.read().option("header", true).option("inferSchema", true)
+                .csv("data/employeeDb.csv").cache();*/
+            datasetRow.printSchema();
+            Dataset<Row> dataset = datasetRow.withColumnRenamed("MERCHANT_ID", "mid")
+                    .withColumnRenamed("ORDER_REF_NUMBER", "orderRefNumber")
+                    .withColumnRenamed("SBI_ORDER_REF_NUMBER", "sbiOrderRefNumber")
+                    .withColumnRenamed("ATRN_NUM", "atrnNum")
+                    .withColumnRenamed("DEBIT_AMT", "debitAmt");
+            dataset.printSchema();
+            return dataset.as(merchantOrderPayment);
+        } catch (Exception ex) {
+            log.info("Error while connect to SQL : {} ", ex.getMessage());
+            throw new RuntimeException(ex.getMessage());
+        }
+    }
