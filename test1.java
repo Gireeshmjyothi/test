683e6d19766d065f0ba255bb564b10a746700971
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-04-24 12:12:19 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,37 +1,10 @@
-public ResponseDto process() {
-        log.info("🚀 POC Application started via CommandLineRunner!");
-        long startTime = System.currentTimeMillis();
-        ResponseDto responseDto = new ResponseDto();
-        List<UnMatchedDto> unMatchedRecords = new ArrayList<>();
-        Dataset<MerchantOrderPayment> datasetRowDb = getDbDataSet();
-        Dataset<MerchantOrderPayment> datasetRowFile = getFileDataset();
-        log.info("Matched");
-        responseDto.setFileRecordCount(datasetRowFile.count());
-        var matchedWithDB = datasetRowFile.join(datasetRowDb)
-                .select(
-                        datasetRowFile.col("atrnNum")
-                );
-        matchedWithDB.show();
-//        matchedWithDB.collectAsList().forEach(employee -> log.info(String.valueOf(employee)));
-        responseDto.setMatchedRecords(matchedWithDB.as(merchantOrderPayment).collectAsList());
-        log.info("Unmatched: updated or added records");
-        var missMatchedWithDB = datasetRowFile.join(datasetRowDb, new String[]{"mid"}, "leftanti").as(Encoders.bean(MerchantOrderPayment.class));
-        missMatchedWithDB.show();
-
-        missMatchedWithDB.collectAsList().forEach(fileRow -> {
-            log.info("File Record: {}", fileRow);
-            Dataset<MerchantOrderPayment> fileRowDs = datasetRowDb.filter("mid = " + fileRow.getMid());
-            UnMatchedDto unMatchedDto = new UnMatchedDto();
-            unMatchedDto.setDbMerchantOrderPayment(fileRow);
-            if (fileRowDs.count() == 0) {
-                log.info("New record: {}", fileRow.getMid());
-                unMatchedDto.setNew(true);
-            } else {
-                log.info("Updated, DB record: {}", fileRowDs.collectAsList());
-            }
-            unMatchedRecords.add(unMatchedDto);
-        });
-        responseDto.setUnMatchedRecords(unMatchedRecords);
-        responseDto.setTimeToProcessed(formatMillis(System.currentTimeMillis() - startTime));
-        return responseDto;
-    }
+responseDto.setMatchedRecords(matchedWithDB.collectAsList());
+responseDto.setFileRecordCount(datasetRowFile.count());
+responseDto.setUnMatchedRecords(
+    missMatchedWithDB.collectAsList().stream().map(fileRow -> {
+        UnMatchedDto unMatchedDto = new UnMatchedDto();
+        unMatchedDto.setDbMerchantOrderPayment(fileRow); // even though it's unmatched, you're storing file data
+        unMatchedDto.setNew(true);
+        return unMatchedDto;
+    }).collect(Collectors.toList())
+);
