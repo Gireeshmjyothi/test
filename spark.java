431130c96d2ba036a8377e553dc6afad6b68e523
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-02 09:20:23 +0530

    Update spark.java

--- spark.java
+++ spark.java
@@ -1,1 +1,26 @@
+	2.	Java JDK 8+ installed and set in JAVA_HOME
 	2.	Spark binary downloaded: https://spark.apache.org/downloads
+	3.	Set environment variables:
+	•	Add Spark bin/ and sbin/ folders to PATH
+	•	Set SPARK_HOME environment variable
+
+		1. Start Master Node
+
+Run this in Command Prompt or PowerShell:
+%SPARK_HOME%\sbin\start-master.cmd
+
+Output will show something like:
+starting org.apache.spark.deploy.master.Master, logging to ...\logs\spark-username-org.apache.spark.deploy.master.Master-1-MACHINE.out
+
+Start a worker process and connect to the master (you’ll get the master URL from step 1 output):
+%SPARK_HOME%\sbin\start-worker.cmd spark://localhost:7077
+
+You’ll see:
+starting org.apache.spark.deploy.worker.Worker, logging to ...\logs\spark-username-org.apache.spark.deploy.worker.Worker-1-MACHINE.out
+
+
+ Stop Master:
+%SPARK_HOME%\sbin\stop-master.cmd
+
+Stop Worker:
+%SPARK_HOME%\sbin\stop-worker.cmd
