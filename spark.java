df7dbe25be2548907ea671cb2a1e8b1980e16951
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-03 18:15:22 +0530

    Update spark.java

--- spark.java
+++ spark.java
@@ -1,3 +1,29 @@
-application with 'debug' enabled.
-18:10:53.565 [main] ERROR org.springframework.boot.SpringApplication - Application run failed
-org.springframework.beans.factory.UnsatisfiedDependencyException: Error creating bean with name 'sparkController' defined in file [C:\Users\v1014352\Downloads\springboot-jsp-spark-demo 2\springboot-jsp-spark-demo\build\classes\java\main\com\rajput\controller\SparkController.class]: Unsatisfied dependency expressed through constructor parameter 0: Error creating bean with name 'sparkService' defined in file [C:\Users\v1014352\Downloads\springboot-jsp-spark-demo 2\springboot-jsp-spark-demo\build\classes\java\main\com\rajput\service\SparkService.class]: Unsatisfied dependency expressed through constructor parameter 0: Error creating bean with name 'sparkSession' defined in class path resource [com/rajput/config/SparkConfig.class]: Failed to instantiate [org.apache.spark.sql.SparkSession]: Factory method 'sparkSession' threw exception with message: Servlet class org.glassfish.jersey.servlet.ServletContainer is not a javax.servlet.Servlet
+
+  starting org.apache.spark.deploy.master.Master, logging to ...\logs\spark-username-org.apache.spark.deploy.master.Master-1-MACHINE.out
+ 
+ Start a worker process and connect to the master (you’ll get the master URL from step 1 output):
+ %SPARK_HOME%\sbin\start-worker.cmd spark://localhost:7077
+ 
+ You’ll see:
+ starting org.apache.spark.deploy.worker.Worker, logging to ...\logs\spark-username-org.apache.spark.deploy.worker.Worker-1-MACHINE.out
+ 
+ implementation('org.apache.spark:spark-core_2.13:3.5.5') {
+     exclude group: 'org.eclipse.jetty'
+     exclude group: 'javax.servlet'
+ }
+ implementation('org.apache.spark:spark-sql_2.13:3.5.5') {
+     exclude group: 'org.eclipse.jetty'
+     exclude group: 'javax.servlet'
+ }
+ 
+ 
+ 
+  Stop Master:
+ %SPARK_HOME%\sbin\stop-master.cmd
+ 
+ Stop Worker:
+ %SPARK_HOME%\sbin\stop-worker.cmd
+ 
+ 
+ java -cp "jars/*" org.apache.spark.deploy.worker.Worker spark://10.30.64.27:7077
+ java -cp "jars/*" org.apache.spark.deploy.master.Master
