07af0c3f70f68309b5bb4642e21f4e6edc6e64e2
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-03 11:44:16 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,89 +1,19 @@
-plugins {
-    id 'java'
-    id 'war'
-    id 'org.springframework.boot' version '3.3.10'
-    id 'io.spring.dependency-management' version '1.1.7'
-}
+spark-shell
+Setting default log level to "WARN".
+To adjust logging level use sc.setLogLevel(newLevel). For SparkR, use setLogLevel(newLevel).
+25/05/03 11:42:50 WARN NativeCodeLoader: Unable to load native-hadoop library for your platform... using builtin-java classes where applicable
+Spark context Web UI available at http://DESKTOP-8ED0TAP.CORP.AD.SBI:4040
+Spark context available as 'sc' (master = local[*], app id = local-1746252771645).
+Spark session available as 'spark'.
+Welcome to
+      ____              __
+     / __/__  ___ _____/ /__
+    _\ \/ _ \/ _ `/ __/  '_/
+   /___/ .__/\_,_/_/ /_/\_\   version 3.5.5
+      /_/
 
-group = 'com.rajput'
-version = '0.0.1'
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
+Using Scala version 2.12.18 (OpenJDK 64-Bit Server VM, Java 21.0.3)
+Type in expressions to have them evaluated.
+Type :help for more information.
 
-repositories {
-    mavenCentral()
-}
-configurations.configureEach {
-    exclude group: 'org.apache.logging.log4j', module: 'log4j-to-slf4j'
-    exclude group: 'ch.qos.logback', module: 'logback-classic'
-    exclude group: 'ch.qos.logback', module: 'logback-core'
-}
-
-dependencies {
-    implementation 'org.springframework.boot:spring-boot-starter-web'
-    providedRuntime 'org.springframework.boot:spring-boot-starter-tomcat'
-    implementation 'org.springframework.boot:spring-boot-starter-data-jpa'
-
-    implementation 'org.apache.tomcat.embed:tomcat-embed-jasper:10.1.20'
-    implementation 'jakarta.servlet.jsp.jstl:jakarta.servlet.jsp.jstl-api:3.0.1'
-    implementation 'org.glassfish.web:jakarta.servlet.jsp.jstl:3.0.1'
-
-    implementation "com.oracle.database.jdbc:ojdbc11:23.5.0.24.07"
-    implementation 'org.antlr:antlr4-runtime:4.9.3'
-
-    implementation "javax.persistence:javax.persistence-api:2.2"
-    implementation 'com.opencsv:opencsv:5.9'
-
-    //Spark-core dependency
-    implementation('org.apache.spark:spark-core_2.13:3.5.5') {
-        exclude group: 'org.eclipse.jetty'
-        exclude group: 'javax.servlet'
-    }
-    implementation('org.apache.spark:spark-sql_2.13:3.5.5') {
-        exclude group: 'org.eclipse.jetty'
-        exclude group: 'javax.servlet'
-    }
-    //Spark-excel dependency
-    implementation 'com.crealytics:spark-excel_2.13:3.5.0_0.20.3'
-
-    implementation 'org.apache.logging.log4j:log4j-api:2.20.0'
-    implementation 'org.apache.logging.log4j:log4j-core:2.20.0'
-    implementation 'org.apache.logging.log4j:log4j-slf4j2-impl:2.20.0'
-
-    compileOnly 'org.projectlombok:lombok:1.18.30'
-    annotationProcessor 'org.projectlombok:lombok:1.18.30'
-
-    testImplementation 'org.springframework.boot:spring-boot-starter-test'
-
-    implementation 'org.apache.logging.log4j:log4j-api:2.20.0'
-    implementation 'org.apache.logging.log4j:log4j-core:2.20.0'
-    implementation 'org.apache.logging.log4j:log4j-slf4j2-impl:2.20.0'
-
-    implementation 'javax.servlet:javax.servlet-api:4.0.1'
-
-
-
-    //mapStruct
-    implementation 'org.mapstruct:mapstruct:1.5.2.Final'
-    annotationProcessor 'org.mapstruct:mapstruct-processor:1.5.2.Final'
-}
-
-tasks.named('test') {
-    useJUnitPlatform()
-}
-
-bootRun {
-    jvmArgs = [
-            "--add-exports", "java.base/sun.nio.ch=ALL-UNNAMED"
-    ]
-}
-war {
-
-}
-//bootWar {
-//    archiveFileName = 'springboot-jsp-spark-demo.war'
-//}
-//java --add-exports=java.base/sun.nio.ch=ALL-UNNAMED -jar build/libs/your-app.war
+scala> 25/05/03 11:43:03 WARN GarbageCollectionMetrics: To enable non-built-in garbage collector(s) List(G1 Concurrent GC), users should configure it(them) to spark.eventLog.gcMetrics.youngGenerationGarbageCollectors or spark.eventLog.gcMetrics.oldGenerationGarbageCollector
