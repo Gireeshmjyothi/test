5fff4f91faed5de772d5f20cead815241e890174
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-03 11:48:05 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,19 +1,7 @@
-spark-shell
-Setting default log level to "WARN".
-To adjust logging level use sc.setLogLevel(newLevel). For SparkR, use setLogLevel(newLevel).
-25/05/03 11:42:50 WARN NativeCodeLoader: Unable to load native-hadoop library for your platform... using builtin-java classes where applicable
-Spark context Web UI available at http://DESKTOP-8ED0TAP.CORP.AD.SBI:4040
-Spark context available as 'sc' (master = local[*], app id = local-1746252771645).
-Spark session available as 'spark'.
-Welcome to
-      ____              __
-     / __/__  ___ _____/ /__
-    _\ \/ _ \/ _ `/ __/  '_/
-   /___/ .__/\_,_/_/ /_/\_\   version 3.5.5
-      /_/
-
-Using Scala version 2.12.18 (OpenJDK 64-Bit Server VM, Java 21.0.3)
-Type in expressions to have them evaluated.
-Type :help for more information.
-
-scala> 25/05/03 11:43:03 WARN GarbageCollectionMetrics: To enable non-built-in garbage collector(s) List(G1 Concurrent GC), users should configure it(them) to spark.eventLog.gcMetrics.youngGenerationGarbageCollectors or spark.eventLog.gcMetrics.oldGenerationGarbageCollector
+        at java.base/java.lang.Thread.run(Thread.java:1583)
+25/05/03 11:35:03 ERROR TransportRequestHandler: Error while invoking RpcHandler#receive() on RPC id 6148087390888286572
+java.io.InvalidClassException: org.apache.spark.rpc.netty.RpcEndpointVerifier$CheckExistence; local class incompatible: stream classdesc serialVersionUID = 5378738997755484868, local class serialVersionUID = 7789290765573734431
+        at java.base/java.io.ObjectStreamClass.initNonProxy(ObjectStreamClass.java:598)
+        at java.base/java.io.ObjectInputStream.readNonProxyDesc(ObjectInputStream.java:2078)
+        at java.base/java.io.ObjectInputStream.readClassDesc(ObjectInputStream.java:1927)
+        at java.base/java.io.ObjectInputStream.readOrdinaryObject(ObjectInputStream.java:2252)
