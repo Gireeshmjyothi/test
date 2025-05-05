aa7247214688a824ac64c0a8159545da1d0812de
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-05 12:23:51 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,8 +1,95 @@
-implementation 'org.springframework.boot:spring-boot-starter-web'
-    providedRuntime 'org.springframework.boot:spring-boot-starter-tomcat' // WAR only
+12:20:30.162 [appclient-register-master-threadpool-0] INFO  org.apache.spark.deploy.client.StandaloneAppClient$ClientEndpoint - Connecting to master spark://10.30.64.27:7077...
+12:20:30.215 [netty-rpc-connection-0] INFO  org.apache.spark.network.client.TransportClientFactory - Successfully created connection to /10.30.64.27:7077 after 35 ms (0 ms spent in bootstraps)
+12:20:30.265 [appclient-register-master-threadpool-0] WARN  org.apache.spark.deploy.client.StandaloneAppClient$ClientEndpoint - Failed to connect to master 10.30.64.27:7077
+org.apache.spark.SparkException: Exception thrown in awaitResult: 
+	at org.apache.spark.util.SparkThreadUtils$.awaitResult(SparkThreadUtils.scala:56) ~[spark-common-utils_2.13-3.5.5.jar:3.5.5]
 
-    // JSP and JSTL (Jakarta compatible)
+
+plugins {
+    id 'java'
+    id 'war'
+    id 'org.springframework.boot' version '3.3.10'
+    id 'io.spring.dependency-management' version '1.1.7'
+}
+
+group = 'com.rajput'
+version = '0.0.1'
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+configurations.configureEach {
+    exclude group: 'org.apache.logging.log4j', module: 'log4j-to-slf4j'
+    exclude group: 'ch.qos.logback', module: 'logback-classic'
+    exclude group: 'ch.qos.logback', module: 'logback-core'
+}
+
+dependencies {
+    implementation 'org.springframework.boot:spring-boot-starter-web'
+    providedRuntime 'org.springframework.boot:spring-boot-starter-tomcat'
+    implementation 'org.springframework.boot:spring-boot-starter-data-jpa'
+
     implementation 'org.apache.tomcat.embed:tomcat-embed-jasper:10.1.20'
     implementation 'jakarta.servlet.jsp.jstl:jakarta.servlet.jsp.jstl-api:3.0.1'
     implementation 'org.glassfish.web:jakarta.servlet.jsp.jstl:3.0.1'
-    implementation 'jakarta.servlet:jakarta.servlet-api:6.0.0'
+
+    implementation "com.oracle.database.jdbc:ojdbc11:23.5.0.24.07"
+
+    implementation "javax.persistence:javax.persistence-api:2.2"
+    implementation 'com.opencsv:opencsv:5.9'
+
+    //Spark-core dependency
+    implementation('org.apache.spark:spark-core_2.12:3.5.5') {
+        exclude group: 'org.eclipse.jetty'
+        exclude group: 'javax.servlet'
+    }
+    implementation('org.apache.spark:spark-sql_2.12:3.5.5') {
+        exclude group: 'org.eclipse.jetty'
+        exclude group: 'javax.servlet'
+    }
+    //Spark-excel dependency
+    implementation 'com.crealytics:spark-excel_2.12:3.5.0_0.20.3'
+
+    implementation 'org.apache.logging.log4j:log4j-api:2.20.0'
+    implementation 'org.apache.logging.log4j:log4j-core:2.20.0'
+    implementation 'org.apache.logging.log4j:log4j-slf4j2-impl:2.20.0'
+
+    compileOnly 'org.projectlombok:lombok:1.18.30'
+    annotationProcessor 'org.projectlombok:lombok:1.18.30'
+
+    testImplementation 'org.springframework.boot:spring-boot-starter-test'
+
+    implementation 'org.apache.logging.log4j:log4j-api:2.20.0'
+    implementation 'org.apache.logging.log4j:log4j-core:2.20.0'
+    implementation 'org.apache.logging.log4j:log4j-slf4j2-impl:2.20.0'
+
+    implementation 'javax.servlet:javax.servlet-api:4.0.1'
+
+
+
+    //mapStruct
+    implementation 'org.mapstruct:mapstruct:1.5.2.Final'
+    annotationProcessor 'org.mapstruct:mapstruct-processor:1.5.2.Final'
+}
+
+tasks.named('test') {
+    useJUnitPlatform()
+}
+
+bootRun {
+    jvmArgs = [
+            "--add-exports", "java.base/sun.nio.ch=ALL-UNNAMED"
+    ]
+}
+war {
+
+}
+//bootWar {
+//    archiveFileName = 'springboot-jsp-spark-demo.war'
+//}
+//java --add-exports=java.base/sun.nio.ch=ALL-UNNAMED -jar build/libs/your-app.war
