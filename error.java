2e75a4e2745eb956af935b44f9d45883ade7a821
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-05 19:45:15 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,17 +1,4 @@
-25/05/05 18:50:13 ERROR ExecutorRunner: Error running executor
-java.lang.IllegalStateException: Cannot find any build directories.
-        at org.apache.spark.launcher.CommandBuilderUtils.checkState(CommandBuilderUtils.java:236)
-        at org.apache.spark.launcher.AbstractCommandBuilder.getScalaVersion(AbstractCommandBuilder.java:241)
-        at org.apache.spark.launcher.AbstractCommandBuilder.buildClassPath(AbstractCommandBuilder.java:195)
-        at org.apache.spark.launcher.AbstractCommandBuilder.buildJavaCommand(AbstractCommandBuilder.java:118)
-        at org.apache.spark.launcher.WorkerCommandBuilder.buildCommand(WorkerCommandBuilder.scala:39)
-        at org.apache.spark.launcher.WorkerCommandBuilder.buildCommand(WorkerCommandBuilder.scala:45)
-        at org.apache.spark.deploy.worker.CommandUtils$.buildCommandSeq(CommandUtils.scala:63)
-        at org.apache.spark.deploy.worker.CommandUtils$.buildProcessBuilder(CommandUtils.scala:51)
-        at org.apache.spark.deploy.worker.ExecutorRunner.org$apache$spark$deploy$worker$ExecutorRunner$$fetchAndRunExecutor(ExecutorRunner.scala:160)
-        at org.apache.spark.deploy.worker.ExecutorRunner$$anon$1.run(ExecutorRunner.scala:80)
-25/05/05 18:50:13 INFO Worker: Executor app-20250505185012-0000/9 finished with state FAILED message java.lang.IllegalStateException: Cannot find any build directories.
-25/05/05 18:50:13 INFO ExternalShuffleBlockResolver: Clean up non-shuffle and non-RDD files associated with the finished executor 9
-25/05/05 18:50:13 INFO ExternalShuffleBlockResolver: Executor is not registered (appId=app-20250505185012-0000, execId=9)
-25/05/05 18:50:13 INFO ExternalShuffleBlockResolver: Application app-20250505185012-0000 removed, cleanupLocalDirs = true
-25/05/05 18:50:13 INFO Worker: Cleaning up local directories for application app-20250505185012-0000
+spark-submit ^ --class com.rajput.SpringbootJspSparkDemoApplication ^ --master spark://10.30.64.27:7077 ^ "C:\Users\v1014352\Downloads\springboot-jsp-spark-demo 2\springboot-jsp-spark-demo\build\libs\springboot-jsp-spark-demo.jar"
+Error: Failed to load class com.rajput.SpringbootJspSparkDemoApplication.
+25/05/05 19:41:55 INFO ShutdownHookManager: Shutdown hook called
+25/05/05 19:41:55 INFO ShutdownHookManager: Deleting directory C:\Users\v1014352\AppData\Local\Temp\spark-5fd3a749-8865-4d70-907e-08ccff39ff54
