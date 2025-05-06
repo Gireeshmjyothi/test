8e35d9052f88f802141c0a5f745ffa3c05cbff6a
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-06 15:54:13 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,16 +1,6 @@
-// Disable bootJar to avoid BOOT-INF layout
-bootJar {
-    enabled = false
-}
-
-jar {
-    enabled = true
-    manifest {
-        attributes(
-            'Main-Class': 'com.rajput.SpringbootJspSparkDemoApplication'
-        )
-    }
-    from {
-        configurations.runtimeClasspath.collect { it.isDirectory() ? it : zipTree(it) }
-    }
-}
+15:51:53.403 [main] INFO  org.apache.spark.sql.internal.SharedState - Warehouse path is 'file:/E:/Gireesh_M/springboot-jsp-spark-demo%202/springboot-jsp-spark-demo/spark-warehouse'.
+15:51:53.659 [stop-spark-context] INFO  org.apache.spark.SparkContext - SparkContext is stopping with exitCode 0.
+15:51:53.666 [stop-spark-context] INFO  org.apache.spark.scheduler.cluster.StandaloneSchedulerBackend - Shutting down all executors
+15:51:53.659 [dispatcher-event-loop-1] ERROR org.apache.spark.rpc.netty.Inbox - Ignoring error
+org.apache.spark.SparkException: Exiting due to error from cluster scheduler: Master removed our application: FAILED
+	at org.apache.spark.errors.SparkCoreErrors$.clusterSchedulerError(SparkCoreErrors.scala:291) ~[spark-core_2.12-3.5.5.jar:3.5.5]
