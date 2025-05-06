0109b3e213298beeb0e03bb955ad31038875f894
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-06 11:05:13 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,8 +1,7 @@
-spark-submit ^ --master spark://10.30.64.27:7077 ^ "C:\Users\v1014352\Downloads\springboot-jsp-spark-demo 2\springboot-jsp-spark-demo\build\libs\springboot-jsp-spark-demo-0.0.1.jar"
-25/05/05 19:56:33 WARN NativeCodeLoader: Unable to load native-hadoop library for your platform... using builtin-java classes where applicable
+spark-submit ^ --class com.rajput.SpringbootJspSparkDemoApplication ^ --master spark://10.30.64.27:7077 ^ "C:\Users\v1014352\Downloads\springboot-jsp-spark-demo 2\springboot-jsp-spark-demo\build\libs\springboot-jsp-spark-demo-0.0.1.war"
 Error: Failed to load class com.rajput.SpringbootJspSparkDemoApplication.
-25/05/05 19:56:34 INFO ShutdownHookManager: Shutdown hook called
-25/05/05 19:56:34 INFO ShutdownHookManager: Deleting directory C:\Users\v1014352\AppData\Local\Temp\spark-e3c4219f-d51b-4f19-9de7-d708d496f393
+25/05/06 11:04:34 INFO ShutdownHookManager: Shutdown hook called
+25/05/06 11:04:34 INFO ShutdownHookManager: Deleting directory C:\Users\v1014352\AppData\Local\Temp\spark-abb7ecfa-ded2-45c5-ba9f-30961ab20a7e
 
 
 jar tf springboot-jsp-spark-demo-0.0.1.jar | grep SpringbootJspSparkDemoApplication
