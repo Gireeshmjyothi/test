34638b902e8b1248df0b7c825c332490b2cf778a
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-06 17:29:17 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,6 +1,4 @@
-15:51:53.403 [main] INFO  org.apache.spark.sql.internal.SharedState - Warehouse path is 'file:/E:/Gireesh_M/springboot-jsp-spark-demo%202/springboot-jsp-spark-demo/spark-warehouse'.
-15:51:53.659 [stop-spark-context] INFO  org.apache.spark.SparkContext - SparkContext is stopping with exitCode 0.
-15:51:53.666 [stop-spark-context] INFO  org.apache.spark.scheduler.cluster.StandaloneSchedulerBackend - Shutting down all executors
-15:51:53.659 [dispatcher-event-loop-1] ERROR org.apache.spark.rpc.netty.Inbox - Ignoring error
-org.apache.spark.SparkException: Exiting due to error from cluster scheduler: Master removed our application: FAILED
-	at org.apache.spark.errors.SparkCoreErrors$.clusterSchedulerError(SparkCoreErrors.scala:291) ~[spark-core_2.12-3.5.5.jar:3.5.5]
+ANTLR Tool version 4.9.3 used for code generation does not match the current runtime version 4.13.0
+ANTLR Runtime version 4.9.3 used for parser compilation does not match the current runtime version 4.13.0
+17:28:16.647 [http-nio-8089-exec-1] ERROR org.apache.catalina.core.ContainerBase.[Tomcat].[localhost].[/].[dispatcherServlet] - Servlet.service() for servlet [dispatcherServlet] in context with path [] threw exception [Handler dispatch failed: java.lang.ExceptionInInitializerError] with root cause
+java.io.InvalidClassException: org.antlr.v4.runtime.atn.ATN; Could not deserialize ATN with version 3 (expected 4).
