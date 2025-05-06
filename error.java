a5af428c1891fa48f72d1f7306c15160e6973d48
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-06 09:12:24 +0530

    Update error.java

--- error.java
+++ error.java
@@ -3,3 +3,18 @@ spark-submit ^ --master spark://10.30.64.27:7077 ^ "C:\Users\v1014352\Downloads\
 Error: Failed to load class com.rajput.SpringbootJspSparkDemoApplication.
 25/05/05 19:56:34 INFO ShutdownHookManager: Shutdown hook called
 25/05/05 19:56:34 INFO ShutdownHookManager: Deleting directory C:\Users\v1014352\AppData\Local\Temp\spark-e3c4219f-d51b-4f19-9de7-d708d496f393
+
+
+jar tf springboot-jsp-spark-demo-0.0.1.jar | grep SpringbootJspSparkDemoApplication
+
+jar xf springboot-jsp-spark-demo-0.0.1.jar META-INF/MANIFEST.MF
+cat META-INF/MANIFEST.MF
+
+Main-Class: com.rajput.SpringbootJspSparkDemoApplication
+
+  spark-submit ^
+  --class com.rajput.SpringbootJspSparkDemoApplication ^
+  --master spark://10.30.64.27:7077 ^
+  "C:\Users\v1014352\Downloads\springboot-jsp-spark-demo 2\springboot-jsp-spark-demo\build\libs\springboot-jsp-spark-demo-0.0.1.jar"
+
+java -jar springboot-jsp-spark-demo-0.0.1.jar
