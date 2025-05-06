6bc25cccc1359b70bc23dfc7f1dafbb563f2b0ea
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-06 12:21:54 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,7 +1,9 @@
-jar {
-    enabled = true
+tasks.register('thinJar', Jar) {
+    group = 'build'
+    description = 'Creates a thin JAR for Spark'
     archiveClassifier.set('thin')
     from sourceSets.main.output
     manifest {
-        attributes 'Main-Class':'com.rajput.SpringbootJspSparkDemoApplication'
+        attributes 'Main-Class': 'com.rajput.SpringbootJspSparkDemoApplication'
     }
+}
