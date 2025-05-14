eab544cf9db5d37e2b3eacf01354b1116f27ad60
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-14 16:06:26 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,1 +1,2 @@
-java.lang.UnsupportedOperationException: java.io.InvalidClassException: org.antlr.v4.runtime.atn.ATN; Could not deserialize ATN with version 3 (expected 4).
+Setting hive.metastore.warehouse.dir ('null') to the value of spark.sql.warehouse.dir.
+2025-05-14 16:03:38.705 INFO | com.epay.rns.service.SparkService:83 | principal=  | scenario= | operation= | correlation= | getDbDataSet | Warehouse path is 'file:/F:/Epay/epay_recon_settlement_service/spark-warehouse'.
