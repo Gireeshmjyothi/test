15d9ed7fec493306e1d8fc2c26118ee397464a51
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-19 17:43:22 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,16 +1,17 @@
-CREATE TABLE YOUR_TABLE_NAME (
-    ID                      RAW(16), -- UUID
-    FILE_SUMMARY_ID         RAW(16), -- UUID
-    ROW_NUMBER              NUMBER,
-    RECORD_TYPE             VARCHAR2(100),
-    ATRN_NUM                VARCHAR2(100),
-    PAYMENT_AMOUNT          NUMBER(15, 2),
-    PAYMENT_DATE            DATE,
-    BANK_REFERENCE_NUMBER   VARCHAR2(100),
-    STATUS                  VARCHAR2(50),
-    RECON_STATUS            VARCHAR2(50),
-    RECON_TIME              TIMESTAMP,
-    SETTLEMENT_STATUS       VARCHAR2(50),
-    SETTLEMENT_TIME         TIMESTAMP,
-    REMARK                  VARCHAR2(500)
-);
+CREATE TABLE RECON_FILE_DTLS
+   (
+       ID                      RAW(16),
+       FILE_SUMMARY_ID         RAW(16),
+       ROW_NUMBER              NUMBER,
+       RECORD_TYPE             VARCHAR2(100),
+       ATRN_NUM                VARCHAR2(100),
+       PAYMENT_AMOUNT          NUMBER(15, 2),
+       PAYMENT_DATE            NUMBER,
+       BANK_REFERENCE_NUMBER   VARCHAR2(100),
+       STATUS                  VARCHAR2(50),
+       RECON_STATUS            VARCHAR2(50),
+       RECON_TIME              TIMESTAMP,
+       SETTLEMENT_STATUS       VARCHAR2(50),
+       SETTLEMENT_TIME         TIMESTAMP,
+       REMARK                  VARCHAR2(500)
+   );
