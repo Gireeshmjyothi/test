42fc519e14ed6bddfacb02175778910f9fc4b247
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-19 20:54:27 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,15 +1,1 @@
-Subject: Discrepancy in Leave Deduction for April – Request for Review
 
-Dear [HR’s Name],
-
-I hope you’re doing well.
-
-I am writing to bring to your attention a discrepancy in my April 2025 leave records. According to my records, I had taken 7 days of leave in April, including applicable sandwich leaves. However, I noticed that 9 days of leave have been deducted.
-
-Additionally, on 7th April, I had a logout miss on the Connecto mobile app, for which I had received manager approval. Despite that, it seems to have been counted towards my leave.
-
-I have attached a screenshot of my Connecto login history for your reference. I kindly request you to review and help rectify the leave count.
-
-Please let me know if you require any further details from my side.
-
-Thank you for your support.
