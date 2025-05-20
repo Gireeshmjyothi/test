2cbd0429adb8e4192d5f2189e9d98b81a1273c36
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-20 16:02:41 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,21 +1,92 @@
-(SELECT * FROM MERCHANT_ORDER_PAYMENTS WHERE CREATED_DATE BETWEEN 1726138792000 AND 1726138793000) AS filtered_data
+package com.epay.rns.service;
 
-Caused by: oracle.jdbc.OracleDatabaseException: ORA-00933: SQL command not properly ended
+import com.sbi.epay.logging.utility.LoggerFactoryUtility;
+import com.sbi.epay.logging.utility.LoggerUtility;
+import lombok.RequiredArgsConstructor;
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.springframework.stereotype.Service;
 
-   String query = String.format("(SELECT * FROM %s WHERE %s) AS filtered_data", tableName, whereClause);
+import java.util.HashMap;
+import java.util.Map;
 
+@Service
+@RequiredArgsConstructor
+public class ReconService {
+    private final LoggerUtility logger = LoggerFactoryUtility.getLogger(this.getClass());
+    private final SparkSession sparkSession;
 
-public Dataset<Row> readFromDBWithFilter(long startMillis, long endMillis, String tableName) {
-        String query = String.format(
-                "SELECT * FROM (SELECT * FROM %s WHERE CREATED_DATE BETWEEN %d AND %d) filtered_data",
-                tableName, startMillis, endMillis
-                );
-        return sparkSession.read()
-                .format("jdbc")
-                .option("url", jdbcConfig.getJdbcUrl())
-                .option("driver", jdbcDriver)
-                .option("dbtable", query)
-                .option("user", jdbcUserName)
-                .option("password", jdbcPassword)
-                .load();
+    private final JdbcReaderService jdbcReaderService;
+
+    public void reconProcess() {
+        logger.info("Recon process start.");
+        String merchantOrderPaymentQuery = String.format(
+                "(SELECT * FROM %s WHERE CREATED_DATE BETWEEN %d AND %d) filtered_data",
+                "MERCHANT_ORDER_PAYMENTS", 1726138792000L, 1726138793000L
+        );
+
+        String reconFileDtlQuery = String.format(
+                "(SELECT * FROM %s WHERE PAYMENT_DATE BETWEEN %d AND %d) filtered_data",
+                "RECON_FILE_DTLS", 1726138792000L, 1726138793000L
+        );
+
+        logger.info("Fetching merchant order payments.");
+        Dataset<Row> merchantOrderPayments = jdbcReaderService.readFromDBWithFilter(merchantOrderPaymentQuery);
+
+        logger.info("Fetching recon file details.");
+        Dataset<Row> reconFileDtls = jdbcReaderService.readFromDBWithFilter(reconFileDtlQuery);
+
+        // Rename columns in source and target to common names
+        for (Map.Entry<String, String> entry : columnMapping().entrySet()) {
+            reconFileDtls = reconFileDtls.withColumnRenamed(entry.getValue(), entry.getKey());
+        }
+
+        // Deduplicate
+        Dataset<Row> merchantOrderDropDuplicate = merchantOrderPayments.dropDuplicates();
+        Dataset<Row> reconDropDuplicate = reconFileDtls.dropDuplicates();
+
+        // Join on key
+        Column joinCond = merchantOrderDropDuplicate.col("ATRN_NUM").equalTo(reconDropDuplicate.col("ATRN_NUM"));
+
+        // Matched rows (key + all mapped column values equal)
+        Column valueMatch = joinCond;
+        for (String col : columnMapping().keySet()) {
+            valueMatch = valueMatch.and(merchantOrderDropDuplicate.col(col).equalTo(reconDropDuplicate.col(col)));
+        }
+
+        Dataset<Row> matched = merchantOrderDropDuplicate.join(reconDropDuplicate, valueMatch, "inner");
+
+        // Unmatched (left anti join to get rows in source not in target)
+        Dataset<Row> unmatched = merchantOrderDropDuplicate.join(reconDropDuplicate, joinCond, "left_anti");
+
+        // Duplicates (if any)
+        Dataset<Row> sourceDuplicates = merchantOrderPayments.except(merchantOrderDropDuplicate);
+        Dataset<Row> targetDuplicates = reconFileDtls.except(reconDropDuplicate);
+
+        System.out.println("--- Matched Rows ---");
+        matched.show();
+
+        System.out.println("--- Unmatched Rows ---");
+        unmatched.show();
+
+        System.out.println("--- Source Duplicates ---");
+        sourceDuplicates.show();
+
+        System.out.println("--- Target Duplicates ---");
+        targetDuplicates.show();
+
+        logger.info("Recon Process End");
     }
+
+    private Map<String, String> columnMapping(){
+        Map<String, String> columnMappings = new HashMap<>();
+        columnMappings.put("PAYMENT_STATUS", "STATUS");
+        columnMappings.put("DEBIT_AMT", "PAYMENT_AMOUNT");
+        columnMappings.put("ATRN_NUM", "ATRN_NUM");
+        columnMappings.put("BANK_REFERENCE_NUMBER", "BANK_REFERENCE_NUMBER");
+        return columnMappings;
+    }
+
+}
