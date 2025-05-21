6e9a27bdec7d8b58e182ae3e1f937477132fab66
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-21 17:07:09 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,11 +1,5 @@
 spark-submit --class com.epay.rns.ReconSettlementServiceApplication --master spark://10.30.64.27:7077 --deploy-mode client --jars "C:\jdbcDriver\ojdbc11-23.5.0.24.07.jar" "F:\Epay\epay_recon_settlement_service\build\libs\epay_recon_settlement_service-0.0.1.jar"
-
-spark-submit --class com.epay.rns.ReconSettlementServiceApplication --master spark://10.30.64.27:7077 --deploy-mode client --jars "C:\jdbcDriver\ojdbc11-23.5.0.24.07.jar" "F:\Epay\epay_recon_settlement_service\build\libs\epay_recon_settlement_service-0.0.1.jar"
-
-
-spark-submit ^
---class com.epay.rns.ReconSettlementServiceApplication ^
---master spark://10.30.64.27:7077 ^
---deploy-mode client ^
---jars "C:\jdbcDriver\ojdbc11-23.5.0.24.07.jar" ^
-"F:\Epay\epay_recon_settlement_service\build\libs\epay_recon_settlement_service-0.0.1.jar"
+25/05/21 17:04:53 WARN NativeCodeLoader: Unable to load native-hadoop library for your platform... using builtin-java classes where applicable
+Error: Failed to load class com.epay.rns.ReconSettlementServiceApplication.
+25/05/21 17:04:53 INFO ShutdownHookManager: Shutdown hook called
+25/05/21 17:04:53 INFO ShutdownHookManager: Deleting directory C:\Users\v1014352\AppData\Local\Temp\spark-f083c83c-97ff-426b-ae62-8d506d3a3c6b
