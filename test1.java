e81b16a33a0c93d80dc93ba31737a351e8b76e52
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-21 17:04:05 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,1 +1,3 @@
-spark-submit ^ --class com.epay.rns.ReconSettlementServiceApplication ^ --master spark://10.30.64.27:7077 --deploy-mode client ^ --jars "C:\jdbcDriver\ojdbc11-23.5.0.24.07.jar" ^ "F:\Epay\epay_recon_settlement_service\build\libs\epay_recon_settlement_service-0.0.1.jar"
+spark-submit --class com.epay.rns.ReconSettlementServiceApplication --master spark://10.30.64.27:7077 --deploy-mode client --jars "C:\jdbcDriver\ojdbc11-23.5.0.24.07.jar" "F:\Epay\epay_recon_settlement_service\build\libs\epay_recon_settlement_service-0.0.1.jar"
+
+spark-submit --class com.epay.rns.ReconSettlementServiceApplication --master spark://10.30.64.27:7077 --deploy-mode client --jars "C:\jdbcDriver\ojdbc11-23.5.0.24.07.jar" "F:\Epay\epay_recon_settlement_service\build\libs\epay_recon_settlement_service-0.0.1.jar"
