240afa80dbf1d85ddb2a69dc034bca5d2705478c
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-22 12:41:21 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,11 +1,4 @@
-private void saveToReconciliationTable(Dataset<Row> dataset, String matchStatus, boolean isMatched) {
-        Dataset<Row> resultDataset = dataset.withColumn("match_status", functions.lit(matchStatus))
-                .withColumn("mismatch_reason", functions.lit(isMatched ? null : "Data mismatch"))
-                .withColumn("source_json", functions.to_json(structFromColumns(dataset.columns())))
-                .withColumn("recon_json", functions.to_json(structFromColumns(dataset.columns())))
-                .withColumn("reconciled_at", functions.current_timestamp())
-                .withColumn("batch_date", functions.current_date());
-
-        resultDataset.printSchema();
-        jdbcReaderService.writeToReconFileResult(resultDataset, "RECONCILIATION_RESULT");
-    }
+ saveToReconciliationTable(matched, "MATCHED", true);
+        saveToReconciliationTable(unmatched, "UNMATCHED", false);
+        saveToReconciliationTable(sourceDuplicates, "SOURCE_DUPLICATE", false);
+        saveToReconciliationTable(targetDuplicates, "TARGET_DUPLICATE", false);
