119e86a10537ca0c1311757ce0627df9ab9455d8
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-22 10:47:20 +0530

    Update spark.java

--- spark.java
+++ spark.java
@@ -66,3 +66,15 @@ private Dataset<Row> getDuplicates(Dataset<Row> dataset, String[] keyCols) {
                   .filter("count > 1")
                   .selectExpr(keyCols);
 }
+private Dataset<Row> getDuplicates(Dataset<Row> dataset, String... keyCols) {
+    Column[] groupCols = Arrays.stream(keyCols)
+                               .map(functions::col)
+                               .toArray(Column[]::new);
+
+    Dataset<Row> duplicates = dataset.groupBy(groupCols)
+                                     .count()
+                                     .filter("count > 1");
+
+    return duplicates.join(dataset, JavaConverters.asScalaBufferConverter(Arrays.asList(keyCols)).asScala().toSeq(), "inner");
+}
+                                   
