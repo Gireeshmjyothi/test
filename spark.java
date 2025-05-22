62606d89589440879c3dfe7f108ad606dd8525af
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-22 09:24:27 +0530

    Update spark.java

--- spark.java
+++ spark.java
@@ -1,29 +1,40 @@
+import org.apache.spark.sql.Column;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.functions;
 
-  starting org.apache.spark.deploy.master.Master, logging to ...\logs\spark-username-org.apache.spark.deploy.master.Master-1-MACHINE.out
- 
- Start a worker process and connect to the master (you’ll get the master URL from step 1 output):
- %SPARK_HOME%\sbin\start-worker.cmd spark://localhost:7077
- 
- You’ll see:
- starting org.apache.spark.deploy.worker.Worker, logging to ...\logs\spark-username-org.apache.spark.deploy.worker.Worker-1-MACHINE.out
- 
- implementation('org.apache.spark:spark-core_2.13:3.5.5') {
-     exclude group: 'org.eclipse.jetty'
-     exclude group: 'javax.servlet'
- }
- implementation('org.apache.spark:spark-sql_2.13:3.5.5') {
-     exclude group: 'org.eclipse.jetty'
-     exclude group: 'javax.servlet'
- }
- 
- 
- 
-  Stop Master:
- %SPARK_HOME%\sbin\stop-master.cmd
- 
- Stop Worker:
- %SPARK_HOME%\sbin\stop-worker.cmd
- 
- 
- java -cp "jars/*" org.apache.spark.deploy.worker.Worker spark://10.30.64.27:7077
- java -cp "jars/*" org.apache.spark.deploy.master.Master
+import java.util.Arrays;
+
+saveToReconciliationTable(matched, "MATCHED", true);
+saveToReconciliationTable(unmatched, "UNMATCHED", false);
+saveToReconciliationTable(sourceDuplicates, "SOURCE_DUPLICATE", false);
+saveToReconciliationTable(targetDuplicates, "TARGET_DUPLICATE", false);
+
+private void saveToReconciliationTable(Dataset<Row> dataset, String matchStatus, boolean isMatched) {
+        Dataset<Row> resultDataset = dataset.withColumn("match_status", functions.lit(matchStatus))
+                .withColumn("mismatch_reason", functions.lit(isMatched ? null : "Data mismatch"))
+                .withColumn("source_json", functions.to_json(structFromColumns(dataset.columns())))
+                .withColumn("recon_json", functions.to_json(structFromColumns(dataset.columns())))
+                .withColumn("reconciled_at", functions.current_timestamp())
+                .withColumn("batch_date", functions.current_date());
+
+        resultDataset.select(
+                        "ATRN_NUM", "match_status", "mismatch_reason",
+                        "source_json", "recon_json", "reconciled_at", "batch_date"
+                ).write()
+                .format("jdbc")
+                .option("url", "jdbc:oracle:thin:@<HOST>:<PORT>:<SID>") // replace with your actual values
+                .option("dbtable", "RECONCILIATION_RESULT")
+                .option("user", "<USERNAME>")
+                .option("password", "<PASSWORD>")
+                .option("driver", "oracle.jdbc.OracleDriver")
+                .mode("append")
+                .save();
+    }
+
+    private Column structFromColumns(String[] columns) {
+        Column[] cols = Arrays.stream(columns)
+                .map(functions::col)
+                .toArray(Column[]::new);
+        return functions.struct(cols);
+    }
