b2b7f58af3457e6c68c18edd90cee9ba316bc9f7
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-22 12:03:14 +0530

    Update spark.java

--- spark.java
+++ spark.java
@@ -67,3 +67,30 @@ public void reconProcess(long startTime, long endTime) {
 
     logger.info("Recon process completed.");
 }
+
+
+ private void saveToReconciliationTable(Dataset<Row> dataset, String matchStatus, boolean isMatched) {
+        Dataset<Row> resultDataset = dataset.withColumn("match_status", functions.lit(matchStatus))
+                .withColumn("mismatch_reason", functions.lit(isMatched ? null : "Data mismatch"))
+                .withColumn("source_json", functions.to_json(structFromColumns(dataset.columns())))
+                .withColumn("recon_json", functions.to_json(structFromColumns(dataset.columns())))
+                .withColumn("reconciled_at", functions.current_timestamp())
+                .withColumn("batch_date", functions.current_date());
+
+
+        jdbcReaderService.writeToReconFileResult(resultDataset, "RECONCILIATION_RESULT");
+    }
+public void writeToReconFileResult(Dataset<Row> df, String tableName) {
+        df.select(
+                        "atrn_num", "match_status", "mismatch_reason",
+                        "source_json", "recon_json", "reconciled_at", "batch_date"
+                ).write()
+                .format("jdbc")
+                .option("url", jdbcConfig.getJdbcUrl())
+                .option("dbtable", tableName)
+                .option("user", jdbcUserName)
+                .option("password", jdbcPassword)
+                .option("driver", jdbcDriver)
+                .mode(SaveMode.Append)
+                .save();
+    }
