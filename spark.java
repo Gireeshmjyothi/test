96a604f4973dd279ad9f0edd1dd6085448e662db
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-23 18:30:51 +0530

    Update spark.java

--- spark.java
+++ spark.java
@@ -1,18 +1,12 @@
 private void saveToReconciliationTable(Dataset<Row> dataset, String matchStatus, String reason) {
-        Dataset<Row> resultDataset = dataset.withColumn("match_status", functions.lit(matchStatus))
-                .withColumn("mismatch_reason", functions.lit(reason))
-                .withColumn("source_json", functions.to_json(structFromColumns(dataset.columns())))
-                .withColumn("recon_json", functions.to_json(structFromColumns(dataset.columns())))
-                .withColumn("reconciled_at", functions.current_timestamp())
-                .withColumn("batch_date", functions.current_date());
+    Dataset<Row> resultDataset = dataset.withColumn("match_status", functions.lit(matchStatus))
+            .withColumn("mismatch_reason", functions.lit(reason))
+            .withColumn("source_json", functions.to_json(structFromColumns(dataset.columns())))
+            .withColumn("recon_json", functions.to_json(structFromColumns(dataset.columns())))
+            // Convert timestamp and date to string to avoid java.lang.IllegalAccessError
+            .withColumn("reconciled_at", functions.date_format(functions.current_timestamp(), "yyyy-MM-dd HH:mm:ss"))
+            .withColumn("batch_date", functions.date_format(functions.current_date(), "yyyy-MM-dd"));
 
-        resultDataset.printSchema();
-        jdbcReaderService.writeToReconFileResult(resultDataset, "RECONCILIATION_RESULT");
-    }
-
-    private Column structFromColumns(String[] columns) {
-        Column[] cols = Arrays.stream(columns)
-                .map(functions::col)
-                .toArray(Column[]::new);
-        return functions.struct(cols);
-    }
+    resultDataset.printSchema();
+    jdbcReaderService.writeToReconFileResult(resultDataset, "RECONCILIATION_RESULT");
+}
