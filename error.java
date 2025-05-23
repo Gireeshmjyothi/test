df1310f29bb17727e2b3493df8262c48b9b1c7bd
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-23 12:54:18 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,2 +1,18 @@
-spark.driver.extraJavaOptions -Djava.io.tmpdir=C:/spark/tmp
-spark.executor.extraJavaOptions -Djava.io.tmpdir=C:/spark/tmp
+
+F:\software\spark-3.5.5-bin-hadoop3>bin\spark-class org.apache.spark.deploy.master.Master
+Using Spark's default log4j profile: org/apache/spark/log4j2-defaults.properties
+25/05/23 12:51:53 INFO Master: Started daemon with process name: 15712@DESKTOP-8ED0TAP
+25/05/23 12:51:53 WARN NativeCodeLoader: Unable to load native-hadoop library for your platform... using builtin-java classes where applicable
+25/05/23 12:51:53 INFO SecurityManager: Changing view acls to: V1014352
+25/05/23 12:51:53 INFO SecurityManager: Changing modify acls to: V1014352
+25/05/23 12:51:53 INFO SecurityManager: Changing view acls groups to:
+25/05/23 12:51:53 INFO SecurityManager: Changing modify acls groups to:
+25/05/23 12:51:53 INFO SecurityManager: SecurityManager: authentication disabled; ui acls disabled; users with view permissions: V1014352; groups with view permissions: EMPTY; users with modify permissions: V1014352; groups with modify permissions: EMPTY
+25/05/23 12:51:54 INFO Utils: Successfully started service 'sparkMaster' on port 7077.
+25/05/23 12:51:54 INFO Master: Starting Spark master at spark://10.30.64.27:7077
+25/05/23 12:51:54 INFO Master: Running Spark version 3.5.5
+25/05/23 12:51:54 INFO JettyUtils: Start Jetty 0.0.0.0:8080 for MasterUI
+25/05/23 12:51:54 INFO Utils: Successfully started service 'MasterUI' on port 8080.
+25/05/23 12:51:54 INFO MasterWebUI: Bound MasterWebUI to 0.0.0.0, and started at http://DESKTOP-8ED0TAP.CORP.AD.SBI:8080
+25/05/23 12:51:54 INFO Master: I have been elected leader! New state: ALIVE
+25/05/23 12:52:13 INFO Master: Registering worker 10.30.64.27:49884 with 12 cores, 14.8 GiB RAM
