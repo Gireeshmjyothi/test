bcfe180d2fd119f466da25dcd466fc18d7d5638a
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-27 11:03:21 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,18 +1,1 @@
-spark:
-  master: local[*]
-  driver:
-    extraJavaOptions: "--add-exports=java.base/sun.util.calendar=ALL-UNNAMED"
-  executor:
-    extraJavaOptions: "--add-exports=java.base/sun.util.calendar=ALL-UNNAMED"
-
-
-
-SparkConf sparkConf = new SparkConf()
-    .setAppName("YourApp")
-    .setMaster("local[*]")
-    .set("spark.driver.extraJavaOptions", "--add-exports=java.base/sun.util.calendar=ALL-UNNAMED")
-    .set("spark.executor.extraJavaOptions", "--add-exports=java.base/sun.util.calendar=ALL-UNNAMED");
-
-SparkSession spark = SparkSession.builder()
-    .config(sparkConf)
-    .getOrCreate();
+Column joinCond = merchantDeduped.col("ATRN_NUM").equalTo(reconDeduped.col("ATRN_NUM"));
