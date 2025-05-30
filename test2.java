429bd23f5c321025d6053ffe25e7cbf434b5852f
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-30 12:40:45 +0530

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,129 +1,22 @@
-plugins {
-	id 'java'
-	id 'org.springframework.boot' version "${spring_boot}"
-	id 'io.spring.dependency-management' version "${dependency_plugin}"
-	id 'com.gorylenko.gradle-git-properties' version "${gorylenko_plugin}"
-}
-
-group = 'com.epay'
-version = "${version}"
-
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
-
-configurations {
-	compileOnly {
-		extendsFrom annotationProcessor
-	}
-}
-configurations.configureEach {
-	exclude group: 'org.apache.logging.log4j', module: 'log4j-to-slf4j'
-	exclude group: 'ch.qos.logback', module: 'logback-classic'
-	exclude group: 'ch.qos.logback', module: 'logback-core'
-}
-
-repositories {
-	mavenCentral()
-	flatDir {
-		dirs "libs"
-	}
-	maven {
-		url "https://gitlab.epay.sbi/api/v4/projects/48/packages/maven"
-		credentials(PasswordCredentials) {
-			username = project.findProperty("gitlab.username")?: System.getenv("CI_USERNAME")
-			password = project.findProperty("gitlab.token")?: System.getenv("CI_JOB_TOKEN")
-		}
-		authentication {
-			basic(BasicAuthentication)
-		}
-	}
-}
-
-dependencies {
-	implementation ('org.springframework.boot:spring-boot-starter-web') {
-		exclude group: 'org.slf4j', module: 'slf4j-log4j12'
-	}
-	implementation 'org.springframework.boot:spring-boot-starter-webflux'
-	implementation 'org.springframework.boot:spring-boot-starter-tomcat'
-	implementation "org.springdoc:springdoc-openapi-starter-webmvc-ui:${swagger}"
-	implementation "org.apache.tomcat.embed:tomcat-embed-jasper:${tomcat_jasper}"
-	implementation "jakarta.servlet.jsp.jstl:jakarta.servlet.jsp.jstl-api:${jakarta_jstl}"
-	implementation "org.glassfish.web:jakarta.servlet.jsp.jstl:${jakarta_jstl}"
-	implementation "javax.servlet:javax.servlet-api:${javax_servlet_api}"
-
-	implementation "com.oracle.database.jdbc:ojdbc11:${oracle_driver}"
-
-	//Spark dependency
-	implementation("org.apache.spark:spark-core_2.13:${apache_spark}") {
-		exclude group: 'org.eclipse.jetty'
-		exclude group: 'org.eclipse.jetty.aggregate'
-		exclude group: 'org.eclipse.jetty.jetty'
-	}
-	implementation("org.apache.spark:spark-sql_2.13:${apache_spark}") {
-		exclude group: 'org.eclipse.jetty'
-	}
-	implementation "software.amazon.awssdk:s3:${aws_s3}"
-
-	implementation "name:logging-service-${epay_logging}"
-
-	//kafka
-	implementation 'org.springframework.kafka:spring-kafka'
-
-	//lombok
-	compileOnly 'org.projectlombok:lombok'
-	annotationProcessor 'org.projectlombok:lombok'
-
-	//mapstruct
-	implementation "org.mapstruct:mapstruct:${mapstruct}"
-	annotationProcessor "org.mapstruct:mapstruct-processor:${mapstruct}"
-
-	//gemfire
-	implementation "com.vmware.gemfire:gemfire-core:${gemfire}"
-	implementation "com.vmware.gemfire:gemfire-cq:${gemfire}"
-	implementation "com.vmware.gemfire:spring-boot-3.3-gemfire-10.1:${gemfire_spring}"
-
-	testImplementation 'org.springframework.boot:spring-boot-starter-test'
-	testRuntimeOnly 'org.junit.platform:junit-platform-launcher'
-}
-
-tasks.named('test') {
-	useJUnitPlatform()
-}
-bootRun {
-	jvmArgs += [
-			"--add-exports", "java.base/sun.nio.ch=ALL-UNNAMED"
-	]
-}
-
-springBoot  {
-	buildInfo()
-}
-
-gitProperties {
-	failOnNoGitDirectory = false // Avoid build failure if .git directory is missing
-	keys = ['git.branch', 'git.commit.id', 'git.commit.time', 'git.commit.id.abbrev']
-}
-//java --add-exports=java.base/sun.nio.ch=ALL-UNNAMED -jar build/libs/your-app.war
-
-
-
-import lombok.*;
-import jakarta.persistence.Id;
-import org.springframework.data.gemfire.mapping.annotation.Region;
-
-import java.io.Serializable;
-
-@Getter
-@Setter
-@Region("Bank_Info")
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class BankInfo implements Serializable {
-    @Id
-
-}
-Cannot resolve symbol 'persistence'
+CREATE TABLE RECON_FILE_SUMMARY
+   (
+        RFS_ID                      RAW(16) PRIMARY KEY,
+        CONFIG_ID                   RAW(16),
+        SFTP_PATH                   VARCHAR2(500),
+        S3_PATH                     VARCHAR2(500),
+        FILE_NAME                   VARCHAR2(50),
+        BANK_ID                     NUMBER,
+        FILE_RECEIVED_TIME          TIMESTAMP,
+        FILE_UPLOAD_TIME            TIMESTAMP,
+        TOTAL_RECORDS               NUMBER,
+        TOTAL_AMOUNT                NUMBER(20,2),
+        PARSING_STATUS              VARCHAR2(50),
+        MATCHED_RECORDS             VARCHAR2(50),
+        UNMATCHED_RECORDS           VARCHAR2(50),
+        DUPLICATE_RECORDS           VARCHAR2(50),
+        RECON_STATUS                VARCHAR2(50),
+        RECON_TIME                  TIMESTAMP,
+        SETTLEMENT_STATUS           VARCHAR2(50),
+        SETTLEMENT_TIME             TIMESTAMP,
+        REMARK                      VARCHAR2(500)
+   );
