11bde0e8112e57562f283d6818cc694ed46f10f3
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-05-31 14:50:37 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,16 +1,30 @@
+// JdbcReaderService.java
+
 public void updateReconFileDetails(Dataset<Row> updateDataset, String tableName, String keyColumn) {
-        updateDataset.createOrReplaceTempView("updates");
+    updateDataset.foreachPartition(iterator -> {
+        try (Connection connection = DriverManager.getConnection("jdbc:your-db-url", "username", "password")) {
+            connection.setAutoCommit(false);
+            String sql = "UPDATE " + tableName + " SET RECON_STATUS = ? WHERE " + keyColumn + " = ?";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                while (iterator.hasNext()) {
+                    Row row = iterator.next();
+                    statement.setString(1, row.getAs("RECON_STATUS"));
+                    statement.setString(2, row.getAs(keyColumn));
+                    statement.addBatch();
+                }
+                statement.executeBatch();
+                connection.commit();
+            } catch (Exception e) {
+                connection.rollback();
+                throw e;
+            }
+        }
+    });
+}
 
-        String updateSQL = String.format(
-                """ 
-                        MERGE INTO %s target
-                        USING updates source
-                        ON target.%s = source.%s
-                        WHEN MATCHED THEN UPDATE SET
-                        target.match_status = source.match_status
-                        """,
-                tableName, keyColumn, keyColumn
-        );
 
-        updateDataset.sparkSession().sql(updateSQL);
-    }
+private void updateReconFileDetails(Dataset<Row> dataset, String status) {
+    Dataset<Row> updated = dataset.select("recon.ATRN_NUM")
+            .withColumn("RECON_STATUS", functions.lit(status));
+    jdbcReaderService.updateReconFileDetails(updated, "RECON_FILE_DTLS", "ATRN_NUM");
+}
