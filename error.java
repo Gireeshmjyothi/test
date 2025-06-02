13eca4502f161f5f7f01f869cd24da2e38b52631
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-06-02 12:25:29 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,196 +1,9 @@
- public void updateReconFileDetails(Dataset<Row> updateDataset, String tableName, String keyColumn) {
-        updateDataset.createOrReplaceTempView("updates");
-
-        String updateSQL = String.format(
-                """ 
-                        MERGE INTO %s target
-                        USING updates source
-                        ON target.%s = source.%s
-                        WHEN MATCHED THEN UPDATE SET
-                        target.RECON_STATUS = source.RECON_STATUS
-                        """,
-                tableName, keyColumn, keyColumn
-        );
-
-        updateDataset.sparkSession().sql(updateSQL);
-    }
-
-
-package com.epay.rns.service;
-
-import com.sbi.epay.logging.utility.LoggerFactoryUtility;
-import com.sbi.epay.logging.utility.LoggerUtility;
-import lombok.RequiredArgsConstructor;
-import org.apache.spark.sql.Column;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.functions;
-import org.springframework.stereotype.Service;
-import scala.collection.JavaConverters;
-
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
-import static java.lang.System.currentTimeMillis;
-import static org.apache.spark.sql.functions.lit;
-
-@Service
-@RequiredArgsConstructor
-public class ReconService {
-    private final LoggerUtility logger = LoggerFactoryUtility.getLogger(this.getClass());
-    private final JdbcReaderService jdbcReaderService;
-    private final SparkService sparkService;
-
-    public void reconProcess(long startTime, long endTime) {
-        logger.info("Recon process started.");
-        long processStartTime = currentTimeMillis();
-        logger.info("🚀 Recon process started at : {} ", sparkService.formatMillis(processStartTime));
-
-        // Load datasets
-        logger.info("🚀 fetch merchant order payments starts : {} ", currentTimeMillis());
-        long localStartTime = currentTimeMillis();
-        Dataset<Row> merchantOrderPayments = readAndNormalize("MERCHANT_ORDER_PAYMENTS", "CREATED_DATE", startTime, endTime).alias("src");
-        logger.info("🚀 fetch merchant order payments ends : {}", sparkService.formatMillis(currentTimeMillis() - localStartTime));
-
-        logger.info("🚀 fetch recon file details starts : {}", currentTimeMillis());
-        localStartTime = currentTimeMillis();
-        Dataset<Row> reconFileDtls = readAndNormalize("RECON_FILE_DTLS", "PAYMENT_DATE", startTime, endTime).alias("tgt");
-        logger.info("🚀 fetch recon file details ends : {}", sparkService.formatMillis(currentTimeMillis() - localStartTime));
-
-        // Rename recon file columns to match source
-        reconFileDtls = renameColumns(reconFileDtls).alias("recon");
-
-        // Join and match logic
-        Column joinCond = reconFileDtls.col("ATRN_NUM").equalTo(merchantOrderPayments.col("ATRN_NUM"))
-                .and(reconFileDtls.col("DEBIT_AMT").equalTo(merchantOrderPayments.col("DEBIT_AMT")));
-
-        Column valueMatch = joinCond;
-        for (String col : columnMapping().keySet()) {
-            valueMatch = valueMatch.and(reconFileDtls.col(col).equalTo(merchantOrderPayments.col(col)));
-        }
-
-        //Filter only unique ATRN_NUMs from reconFileDtls
-        Dataset<Row> reconUnique = reconFileDtls.groupBy("ATRN_NUM")
-                .count()
-                .filter("count = 1")
-                .select("ATRN_NUM");
-
-        Dataset<Row> filteredRecon = reconFileDtls.join(reconUnique, "ATRN_NUM");
-
-        //Join unique-only records to find matched ones
-        Column matchCondition = filteredRecon.col("ATRN_NUM").equalTo(merchantOrderPayments.col("ATRN_NUM"))
-                .and(filteredRecon.col("DEBIT_AMT").equalTo(merchantOrderPayments.col("DEBIT_AMT")));
-
-        // Matched rows
-        logger.info("🚀 Fetch matched data starts: {}", currentTimeMillis());
-        localStartTime = currentTimeMillis();
-        Dataset<Row> matched = filteredRecon
-                .join(merchantOrderPayments, matchCondition, "inner")
-                .dropDuplicates("ATRN_NUM");
-        logger.info("🚀 Fetch matched data ends: {}", sparkService.formatMillis(currentTimeMillis() - localStartTime));
-
-        // Unmatched rows from reconFileDtls not in merchantOrderPayments
-        logger.info("🚀 Fetch unmatched data starts: {}", sparkService.formatMillis(currentTimeMillis()));
-        localStartTime = currentTimeMillis();
-        Dataset<Row> unmatched = reconFileDtls.join(merchantOrderPayments, joinCond, "left_anti").distinct();
-        logger.info("🚀 Fetch unmatched data ends: {}", sparkService.formatMillis(currentTimeMillis() - localStartTime));
-
-        // Duplicates in reconFileDtls
-        logger.info("🚀 Fetch duplicate data starts: {}", sparkService.formatMillis(currentTimeMillis()));
-        localStartTime = currentTimeMillis();
-        Dataset<Row> reconFileDetailDuplicate = getDuplicates(reconFileDtls, "ATRN_NUM");
-        logger.info("🚀 Fetch duplicate data ends: {}", sparkService.formatMillis(currentTimeMillis() - localStartTime));
-
-        // Update match_status back in RECON_FILE_DTLS
-        logger.info("🚀 Update process data starts: {}", sparkService.formatMillis(currentTimeMillis()));
-        localStartTime = currentTimeMillis();
-        updateReconFileDetails(matched, "MATCHED");
-        updateReconFileDetails(unmatched, "UNMATCHED");
-        updateReconFileDetails(reconFileDetailDuplicate, "DUPLICATE");
-        logger.info("🚀 Update process data ends: {}", sparkService.formatMillis(currentTimeMillis() - localStartTime));
-
-        logger.info("Recon process completed in: {}", sparkService.formatMillis(currentTimeMillis() - processStartTime));
-    }
-
-    private Dataset<Row> getDuplicates(Dataset<Row> dataset, String... keyCols) {
-        Column[] groupCols = Arrays.stream(keyCols).map(functions::col).toArray(Column[]::new);
-        Dataset<Row> duplicates = dataset.groupBy(groupCols).count().filter("count > 1");
-        return duplicates.join(dataset, JavaConverters.asScalaBufferConverter(Arrays.asList(keyCols)).asScala().toSeq(), "inner");
-    }
-
-    private Dataset<Row> readAndNormalize(String tableName, String dateColumn, long startTime, long endTime) {
-        String query = buildQuery(tableName, dateColumn, startTime, endTime);
-        logger.info("Executing query for table '{}': {}", tableName, query);
-        return normalize(jdbcReaderService.readFromDBWithFilter(tableName));
-    }
-
-    private String buildQuery(String tableName, String dateColumn, long startTime, long endTime) {
-        return String.format("(SELECT * FROM %s WHERE %s BETWEEN %d AND %d) filtered_data", tableName, dateColumn, startTime, endTime);
-    }
-
-    private Dataset<Row> normalize(Dataset<Row> dataset) {
-        for (String col : columnMapping().keySet()) {
-            if (Arrays.asList(dataset.columns()).contains(col)) {
-                dataset = dataset.withColumn(col, functions.upper(functions.trim(dataset.col(col))));
-            }
-        }
-        if (Arrays.asList(dataset.columns()).contains("ATRN_NUM")) {
-            dataset = dataset.withColumn("ATRN_NUM", functions.upper(functions.trim(dataset.col("ATRN_NUM"))));
-        }
-        return dataset;
-    }
-
-    private Dataset<Row> renameColumns(Dataset<Row> dataset) {
-        for (Map.Entry<String, String> entry : columnMapping().entrySet()) {
-            if (Arrays.asList(dataset.columns()).contains(entry.getValue())) {
-                dataset = dataset.withColumnRenamed(entry.getValue(), entry.getKey());
-            }
-        }
-        return dataset;
-    }
-
-    private Map<String, String> columnMapping() {
-        Map<String, String> columnMappings = new HashMap<>();
-        columnMappings.put("DEBIT_AMT", "PAYMENT_AMOUNT");
-        columnMappings.put("ATRN_NUM", "ATRN_NUM");
-        columnMappings.put("PAYMENT_STATUS", "STATUS");
-
-        return columnMappings;
-    }
-
-    private void saveToReconciliationTable(Dataset<Row> dataset, String matchStatus, String reason) {
-        Date currentTimeStamp = Date.valueOf(LocalDateTime.now().toLocalDate());
-        Timestamp batchTime = Timestamp.valueOf(LocalDateTime.now());
-        Dataset<Row> resultDataset = dataset
-                .withColumn("match_status", lit(matchStatus))
-                .withColumn("mismatch_reason", lit(reason))
-                .withColumn("source_json", functions.to_json(functions.struct(Arrays.stream(dataset.columns()).map(functions::col).toArray(Column[]::new))))
-                .withColumn("recon_json", functions.to_json(functions.struct(Arrays.stream(dataset.columns()).map(functions::col).toArray(Column[]::new))))
-                .withColumn("reconciled_at", lit(currentTimeStamp))
-                .withColumn("batch_date", lit(batchTime))
-                .repartition(4);
-        jdbcReaderService.writeToReconFileResult(resultDataset, "RECONCILIATION_RESULT");
-    }
-
-    private void updateReconFileDetails(Dataset<Row> dataset, String status) {
-        Dataset<Row> updated = dataset.select("recon.ATRN_NUM")
-                .withColumn("RECON_STATUS", lit(status));
-
-        jdbcReaderService.updateReconFileDetails(updated, "RECON_FILE_DTLS", "ATRN_NUM");
-    }
-}
-
-
  Request: http://localhost:9097/api/rns/v1/process raised {}
-java.lang.RuntimeException: java.sql.SQLSyntaxErrorException: ORA-00969: missing ON keyword
+java.lang.RuntimeException: java.sql.SQLSyntaxErrorException: ORA-00928: missing SELECT keyword
 
-https://docs.oracle.com/error-help/db/ora-00969/
+https://docs.oracle.com/error-help/db/ora-00928/
 	at com.epay.rns.service.JdbcReaderService.mergeStageToMain(JdbcReaderService.java:132)
-	at com.epay.rns.service.ReconService.updateReconFileDetails(ReconService.java:169)
+	at com.epay.rns.service.ReconService.updateReconFileDetails(ReconService.java:168)
 	at com.epay.rns.service.ReconService.reconProcess(ReconService.java:93)
 	at com.epay.rns.controller.SparkController.reconProcess(SparkController.java:31)
 	at java.base/jdk.internal.reflect.DirectMethodHandleAccessor.invoke(DirectMethodHandleAccessor.java:103)
@@ -199,49 +12,44 @@ https://docs.oracle.com/error-help/db/ora-00969/
 	at org.springframework.web.method.support.InvocableHandlerMethod.invokeForRequest(InvocableHandlerMethod.java:188)
 	at org.springframework.web.servlet.mvc.method.annotation.ServletInvocableHandlerMethod.invokeAndHandle(ServletInvocableHandlerMethod.java:118)
 	at org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter.invokeHandlerMethod(RequestMappingHandlerAdapter.java:926)
-	at org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter.handleInternal(RequestMappingHandlerAdapter.java:831)
-	at org.springframework.web.servlet.mvc.method.AbstractHandlerMethodAdapter.handle(AbstractHandlerMethodAdapter.java:87)
-	at org.springframework.web.servlet.DispatcherServlet.doDispatch(DispatcherServlet.java:1089)
-	at org.springframework.web.servlet.DispatcherServlet.doService(DispatcherServlet.java:979)
-	at org.springframework.web.servlet.FrameworkServlet.processRequest(FrameworkServlet.java:1014)
-	at org.springframework.web.servlet.FrameworkServlet.doGet(FrameworkServlet.java:903)
-	at jakarta.servlet.http.HttpServlet.service(HttpServlet.java:564)
-	at org.springframework.web.servlet.FrameworkServlet.service(FrameworkServlet.java:885)
-	at jakarta.servlet.http.HttpServlet.service(HttpServlet.java:658)
-	at org.apache.catalina.core.ApplicationFilterChain.internalDoFilter(ApplicationFilterChain.java:195)
-	at org.apache.catalina.core.ApplicationFilterChain.doFilter(ApplicationFilterChain.java:140)
-	at org.apache.tomcat.websocket.server.WsFilter.doFilter(WsFilter.java:51)
-	at org.apache.catalina.core.ApplicationFilterChain.internalDoFilter(ApplicationFilterChain.java:164)
-	at org.apache.catalina.core.ApplicationFilterChain.doFilter(ApplicationFilterChain.java:140)
-	at org.springframework.web.filter.RequestContextFilter.doFilterInternal(RequestContextFilter.java:100)
-	at org.springframework.web.filter.OncePerRequestFilter.doFilter(OncePerRequestFilter.java:116)
-	at org.apache.catalina.core.ApplicationFilterChain.internalDoFilter(ApplicationFilterChain.java:164)
-	at org.apache.catalina.core.ApplicationFilterChain.doFilter(ApplicationFilterChain.java:140)
-	at org.springframework.web.filter.FormContentFilter.doFilterInternal(FormContentFilter.java:93)
-	at org.springframework.web.filter.OncePerRequestFilter.doFilter(OncePerRequestFilter.java:116)
-	at org.apache.catalina.core.ApplicationFilterChain.internalDoFilter(ApplicationFilterChain.java:164)
-	at org.apache.catalina.core.ApplicationFilterChain.doFilter(ApplicationFilterChain.java:140)
-	at org.springframework.web.filter.CharacterEncodingFilter.doFilterInternal(CharacterEncodingFilter.java:201)
-	at org.springframework.web.filter.OncePerRequestFilter.doFilter(OncePerRequestFilter.java:116)
-	at org.apache.catalina.core.ApplicationFilterChain.internalDoFilter(ApplicationFilterChain.java:164)
-	at org.apache.catalina.core.ApplicationFilterChain.doFilter(ApplicationFilterChain.java:140)
-	at org.apache.catalina.core.StandardWrapperValve.invoke(StandardWrapperValve.java:167)
-	at org.apache.catalina.core.StandardContextValve.invoke(StandardContextValve.java:90)
-	at org.apache.catalina.authenticator.AuthenticatorBase.invoke(AuthenticatorBase.java:483)
-	at org.apache.catalina.core.StandardHostValve.invoke(StandardHostValve.java:115)
-	at org.apache.catalina.valves.ErrorReportValve.invoke(ErrorReportValve.java:93)
-	at org.apache.catalina.core.StandardEngineValve.invoke(StandardEngineValve.java:74)
-	at org.apache.catalina.connector.CoyoteAdapter.service(CoyoteAdapter.java:344)
-	at org.apache.coyote.http11.Http11Processor.service(Http11Processor.java:397)
-	at org.apache.coyote.AbstractProcessorLight.process(AbstractProcessorLight.java:63)
-	at org.apache.coyote.AbstractProtocol$ConnectionHandler.process(AbstractProtocol.java:905)
-	at org.apache.tomcat.util.net.NioEndpoint$SocketProcessor.doRun(NioEndpoint.java:1743)
-	at org.apache.tomcat.util.net.SocketProcessorBase.run(SocketProcessorBase.java:52)
-	at org.apache.tomcat.util.threads.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1190)
-	at org.apache.tomcat.util.threads.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:659)
-	at org.apache.tomcat.util.threads.TaskThread$WrappingRunnable.run(TaskThread.java:63)
-	at java.base/java.lang.Thread.run(Thread.java:1583)
-Caused by: java.sql.SQLSyntaxErrorException: ORA-00969: missing ON keyword
 
-https://docs.oracle.com/error-help/db/ora-00969/
-Caused by: java.sql.SQLSyntaxErrorException: ORA-00969: missing ON keyword
+
+private void updateReconFileDetails(Dataset<Row> dataset, String status) {
+        Dataset<Row> updated = dataset.select(functions.col("recon.ATRN_NUM"))
+                .withColumn("RECON_STATUS", functions.lit(status));
+        updated.printSchema();
+        updated.first();
+        String stageTableName = "RECON_FILE_DTLS_STAGE";
+        jdbcReaderService.writeToStageTable(updated, stageTableName);
+        jdbcReaderService.mergeStageToMain(stageTableName, "RECON_FILE_DTLS", "ATRN_NUM");
+    }
+
+public void writeToStageTable(Dataset<Row> updatedData, String stageTableName) {
+        updatedData.write()
+                .mode(SaveMode.Overwrite)
+                .format("jdbc")
+                .option("url", jdbcUrl)
+                .option("dbtable", stageTableName)
+                .option("user", jdbcUserName)
+                .option("password", jdbcPassword)
+                .save();
+    }
+
+    public void mergeStageToMain(String stageTableName, String targetTableName, String keyColumn) {
+        String mergeSql = String.format(
+                "(MERGE INTO %s T " +
+                        "USING %s S " +
+                        "ON T.%s = S.%s " +
+                        "WHEN MATCHED THEN UPDATE SET T.RECON_STATUS = S.RECON_STATUS)",
+                targetTableName, stageTableName, keyColumn, keyColumn
+        );
+
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, jdbcUserName, jdbcPassword);
+             Statement stmt = connection.createStatement()) {
+            stmt.execute(mergeSql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
