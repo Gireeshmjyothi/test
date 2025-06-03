2b2768a2e420bd2d06fbc782897fc429aed8facb
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-06-03 17:04:46 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,5 +1,42 @@
-CREATE TABLE RECON_FILE_DTLS_STAGE (
-    RFD_ID RAW(16) PRIMARY KEY,
-    ATRN_NUM VARCHAR2(100),
-    RECON_STATUS VARCHAR2(50)
-)
+import java.nio.ByteBuffer;
+import java.util.UUID;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.api.java.UDF1;
+
+import static org.apache.spark.sql.functions.callUDF;
+import static org.apache.spark.sql.functions.lit;
+import static org.apache.spark.sql.functions.col;
+
+// Register UDF once in your service initialization
+public void registerUuidToBytesUdf() {
+    spark.udf().register("uuidToBytes", (UDF1<String, byte[]>) uuid -> {
+        UUID u = UUID.fromString(uuid);
+        byte[] buffer = new byte[16];
+        ByteBuffer bb = ByteBuffer.wrap(buffer);
+        bb.putLong(u.getMostSignificantBits());
+        bb.putLong(u.getLeastSignificantBits());
+        return buffer;
+    }, DataTypes.BinaryType);
+}
+
+
+
+public void stageReconStatus(Dataset<Row> dataset, String status) {
+    Dataset<Row> staged = dataset
+            .withColumn("RFD_ID", callUDF("uuidToBytes", col("RFD_ID")))
+            .withColumn("RECON_STATUS", lit(status))
+            .dropDuplicates("RFD_ID");
+
+    staged.write()
+            .mode(SaveMode.Append)
+            .format("jdbc")
+            .option("url", jdbcUrl)
+            .option("dbtable", "RECON_FILE_DTLS_STAGE")
+            .option("user", jdbcUserName)
+            .option("password", jdbcPassword)
+            .option("driver", jdbcDriver)
+            .save();
+}
