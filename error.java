a31b8c50c0107982c8ae9bfcab1bf3089e6a51a4
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-06-10 18:45:24 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,24 +1,3 @@
-private void publishToKafka(Dataset<Row> dataset, String topic) {
-    if (dataset.isEmpty()) {
-        logger.info("Dataset is empty. Skipping Kafka publish for topic: {}", topic);
-        return;
-    }
+Job aborted due to stage failure: Task 0 in stage 11.0 failed 1 times, most recent failure: Lost task 0.0 in stage 11.0 (TID 6) (DESKTOP-8ED0TAP.CORP.AD.SBI executor driver): org.apache.kafka.common.errors.TimeoutException: Topic matched-recon-file-details-dev not present in metadata after 60000 ms.
 
-    try {
-        Dataset<Row> kafkaDf = dataset.selectExpr(
-            "CAST(RFD_ID AS STRING) AS key", // Kafka requires key/value as STRING or BINARY
-            "to_json(named_struct('RFD_ID', CAST(RFD_ID AS STRING), 'ATRN_NUM', recon.ATRN_NUM)) AS value"
-        );
-
-        kafkaDf.write()
-               .format("kafka")
-               .option("kafka.bootstrap.servers", "dev-cluster-kafka-bootstrap-dev-kafka.apps.dev.sbiepay.sbi:443")
-               .option("topic", topic)
-               .save();
-
-        logger.info("Successfully published {} records to Kafka topic '{}'.", dataset.count(), topic);
-
-    } catch (Exception e) {
-        logger.error("Failed to publish data to Kafka topic '{}': {}", topic, e.getMessage(), e);
-    }
-}
+Driver stacktrace:
