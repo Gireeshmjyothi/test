21024163f5ad0925a4e5f4eff9dc7f2753fe592b
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-06-10 18:55:31 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,3 +1,8 @@
-Job aborted due to stage failure: Task 0 in stage 11.0 failed 1 times, most recent failure: Lost task 0.0 in stage 11.0 (TID 6) (DESKTOP-8ED0TAP.CORP.AD.SBI executor driver): org.apache.kafka.common.errors.TimeoutException: Topic matched-recon-file-details-dev not present in metadata after 60000 ms.
-
-Driver stacktrace:
+// Subscribe to multiple topics
+Dataset<Row> df = spark
+  .readStream()
+  .format("kafka")
+  .option("kafka.bootstrap.servers", "host1:port1,host2:port2")
+  .option("subscribe", "topic1,topic2")
+  .load();
+df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)");
