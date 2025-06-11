350f4e07e9ba918adadbc90aea8044f2c00be6fc
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-06-11 18:38:00 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,21 +1,8 @@
-import java.util.UUID;
-
-public class UuidConverter {
-    public static UUID convertStringToUuid(String uuidStrWithoutHyphens) {
-        if (uuidStrWithoutHyphens.length() != 32) {
-            throw new IllegalArgumentException("Invalid UUID string format");
-        }
-
-        String formatted = uuidStrWithoutHyphens.replaceFirst(
-            "(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)",
-            "$1-$2-$3-$4-$5"
-        );
-        return UUID.fromString(formatted);
-    }
-
-    public static void main(String[] args) {
-        String uuidStr = "7f9c1f31f3b14e88b9cbe7df2c9e1b2d";
-        UUID uuid = convertStringToUuid(uuidStr);
-        System.out.println("UUID: " + uuid);
-    }
-}
+Dataset<Row> kafkaDf = dataset.selectExpr(
+    "upper(hex(RFD_ID)) AS key",
+    "to_json(named_struct(" +
+        "'RFD_ID', upper(hex(RFD_ID)), " +
+        "'ATRN_NUM', ATRN_NUM, " +
+        "'STATUS', '" + status + "'" +  // dynamically inject status
+    ")) AS value"
+);
