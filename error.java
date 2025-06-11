7892aee9b787a927bb997a0f60521d57f6f0f0c3
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-06-11 11:48:25 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,8 +1,20 @@
-// Subscribe to multiple topics
-Dataset<Row> df = spark
-  .readStream()
-  .format("kafka")
-  .option("kafka.bootstrap.servers", "host1:port1,host2:port2")
-  .option("subscribe", "topic1,topic2")
-  .load();
-df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)");
+private void publishToKafka(Dataset<Row> dataset, String topic) {
+        try {
+            Dataset<Row> kafkaDf = dataset.selectExpr(
+                    "RFD_ID AS key",
+                    "to_json(named_struct('RFD_ID', RFD_ID, 'ATRN_NUM', recon.ATRN_NUM)) AS value"
+            );
+            kafkaDf.printSchema();
+            kafkaDf.describe();
+            kafkaDf.write()
+                    .format("kafka")
+                    .option("kafka.bootstrap.servers", "dev-cluster-kafka-bootstrap-dev-kafka.apps.dev.sbiepay.sbi:443")
+                    .option("topic", topic)
+                    .save();
+
+            logger.info("Successfully published records to Kafka topic '{}'.", topic);
+
+        } catch (Exception e) {
+            logger.error("Failed to publish data to Kafka topic '{}': {}", topic, e.getMessage());
+        }
+    }
