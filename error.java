e131b6682f034ffe60c41bf34c2b16112f2c4ebb
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-06-13 16:55:19 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,1 +1,6 @@
-
+ @Query(value = """
+            SELECT RFS_ID, RECON_STATUS, COUNT(*)
+            FROM RECON_FILE_DTLS
+            WHERE RFS_ID = :rfsId group by RFS_ID, RECON_STATUS
+            """, nativeQuery = true)
+    Object findReconStatusCountByRfsId(@Param("rfsId") UUID rfsId);
