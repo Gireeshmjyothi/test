df6d150829e1bf4fdac99b98146f517c9df0da33
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-06-21 10:55:39 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,25 +1,25 @@
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.expressions.Window;
-import org.apache.spark.sql.expressions.WindowSpec;
-import static org.apache.spark.sql.functions.*;
+public void writeToStagingTable(Dataset<Row> dataset, String tableName) {
+        logger.info("Inserting into recon status stage.");
+        Properties connectionProperties = new Properties();
+        connectionProperties.put("user", jdbcUserName);
+        connectionProperties.put("password", jdbcPassword);
+        connectionProperties.put("driver", jdbcDriver);
 
-public Dataset<Row> getOneMatchedPerGroup(Dataset<Row> reconFileDtls, Dataset<Row> merchantDtls) {
-    // Step 1: Join on atrnNum and debitAmt
-    Dataset<Row> matched = reconFileDtls
-        .join(merchantDtls,
-            reconFileDtls.col("atrnNum").equalTo(merchantDtls.col("atrnNum"))
-                .and(reconFileDtls.col("debitAmt").equalTo(merchantDtls.col("debitAmt"))),
-            "inner"
-        )
-        .select(reconFileDtls.col("rfdId"), reconFileDtls.col("atrnNum"), reconFileDtls.col("debitAmt"));
+        dataset.write()
+                .mode(SaveMode.Append)
+                .jdbc(jdbcUrl, tableName, connectionProperties);
+        logger.info("Insertion completed.");
+    }
 
-    // Step 2: Keep only one record per (atrnNum, debitAmt)
-    WindowSpec windowSpec = Window.partitionBy("atrnNum", "debitAmt").orderBy("rfdId");
-    Dataset<Row> onePerGroup = matched
-        .withColumn("row_num", row_number().over(windowSpec))
-        .filter(col("row_num").equalTo(1))
-        .drop("row_num");
 
-    return onePerGroup;
-}
+    public void updateReconStatusFromStage() {
+        String mergeSql = """
+                    MERGE INTO RECON_FILE_DTLS tgt
+                    USING RECON_STATUS_STAGE src
+                    ON (tgt.RFD_ID = src.RFD_ID)
+                    WHEN MATCHED THEN
+                      UPDATE SET tgt.RECON_STATUS = src.RECON_STATUS
+                """;
+
+        jdbcTemplate.update(mergeSql);
+    }
