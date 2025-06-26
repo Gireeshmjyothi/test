c2e1257f2ed86bcf9e969ef47ff12d09e22dc417
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-06-26 14:26:11 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,53 +1,6 @@
-private Dataset<Row>[] classifyReconData(Dataset<Row> reconFileDataset, Dataset<Row> transactionDataset) {
-    logger.info("Starting classification of recon data...");
-
-    // Step 0: Alias
-    Dataset<Row> recon = reconFileDataset.alias("recon");
-    Dataset<Row> txn = transactionDataset.alias("txn");
-
-    // Step 1: Find matched records
-    Dataset<Row> matched = recon
-            .join(txn,
-                    col("recon.ATRN_NUM").equalTo(col("txn.ATRN_NUM"))
-                            .and(col("recon.DEBIT_AMT").equalTo(col("txn.DEBIT_AMT"))),
-                    "inner")
-            .select(
-                    col("recon.rfdId"),
-                    col("recon.ATRN_NUM"),
-                    col("recon.DEBIT_AMT")
-            )
-            .withColumn("RECON_STATUS", lit("MATCHED"));
-
-    // Step 2: Exclude matched rows from recon dataset
-    Dataset<Row> unmatchedOrDuplicate = recon
-            .join(matched,
-                    recon.col("ATRN_NUM").equalTo(matched.col("ATRN_NUM"))
-                            .and(recon.col("DEBIT_AMT").equalTo(matched.col("DEBIT_AMT")))
-                            .and(recon.col("rfdId").equalTo(matched.col("rfdId"))),
-                    "left_anti")
-            .select(
-                    col("recon.rfdId"),
-                    col("recon.ATRN_NUM"),
-                    col("recon.DEBIT_AMT")
-            );
-
-    // Step 3: Classify unmatched and duplicates using row_number
-    WindowSpec windowSpec = Window.partitionBy("ATRN_NUM", "DEBIT_AMT").orderBy("DEBIT_AMT");
-
-    Dataset<Row> unmatchedAndDuplicate = unmatchedOrDuplicate
-            .withColumn("row_num", row_number().over(windowSpec))
-            .withColumn("RECON_STATUS",
-                    when(col("row_num").equalTo(1), lit("UNMATCHED"))
-                            .otherwise(lit("DUPLICATE")))
-            .drop("row_num");
-
-    // Step 4: Combine all
-    Dataset<Row> finalDataset = matched.unionByName(unmatchedAndDuplicate);
-
-    // Step 5: Return split datasets
-    return new Dataset[]{
-            finalDataset.filter(col("RECON_STATUS").equalTo("MATCHED")),
-            finalDataset.filter(col("RECON_STATUS").equalTo("DUPLICATE")),
-            finalDataset.filter(col("RECON_STATUS").equalTo("UNMATCHED"))
-    };
-}
+/**
+     * This method is used to truncate stage table.
+     */
+    public void clearStageTable() {
+        jdbcTemplate.update("TRUNCATE TABLE RECON_STATUS_STAGE");
+    }
