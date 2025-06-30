ebe07dae2064b31c2b7a3e444618d296040509fd
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-06-30 18:49:07 +0530

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,15 +1,16 @@
-Dataset<Row> matchedAtrns = reconFileDataset.alias("recon")
-    .join(transactionDataset.alias("txn"),
-          col("recon.ATRN_NUM").equalTo(col("txn.ATRN_NUM"))
-              .and(col("recon.DEBIT_AMT").equalTo(col("txn.DEBIT_AMT"))),
-          "inner")
-    .select(col("recon.ATRN_NUM").alias("MATCHED_ATRN"),
-            col("recon.DEBIT_AMT").alias("MATCHED_DEBIT_AMT"),
-            functions.row_number().over(Window.partitionBy("recon.ATRN_NUM").orderBy(/* your logic */)).alias("match_rank"));
+Dataset<Row> matchedAtrns = joined
+    .filter(col("exactMatch").equalTo(1))
+    .select(col("recon.ATRN_NUM"), col("recon.DEBIT_AMT"), col("recon.RFD_ID"))
+    .distinct()
+    .withColumnRenamed("ATRN_NUM", "MATCHED_ATRN")
+    .withColumnRenamed("DEBIT_AMT", "MATCHED_DEBIT_AMT")
+    .withColumnRenamed("RFD_ID", "MATCHED_RFD_ID");
+
 
 Dataset<Row> withMatchedFlag = joined
     .join(matchedAtrns,
-          col("recon.ATRN_NUM").equalTo(col("MATCHED_ATRN"))
-              .and(col("recon.DEBIT_AMT").equalTo(col("MATCHED_DEBIT_AMT"))),
-          "left_outer")
+        col("recon.ATRN_NUM").equalTo(col("MATCHED_ATRN"))
+            .and(col("recon.DEBIT_AMT").equalTo(col("MATCHED_DEBIT_AMT")))
+            .and(col("recon.RFD_ID").equalTo(col("MATCHED_RFD_ID"))),
+        "left_outer")
     .withColumn("isAtrnMatched", col("MATCHED_ATRN").isNotNull());
