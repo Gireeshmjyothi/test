e1a97fb3e908ce84a7e438c8d17c3cf723a4964d
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-07-02 12:47:32 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,4 +1,29 @@
-public static String buildQueryForTransaction(UUID rfsId){
-        String hexUUID = rfsId.toString().replace("-", "").toUpperCase();
-        return String.format("(SELECT m.* FROM MERCHANT_ORDER_PAYMENTS m, RECON_FILE_DTLS r WHERE r.ATRN_NUM = m.ATRN_NUM AND r.RFS_ID = HEXTORAW('%s'))", hexUUID);
+package com.example.sftpupload.service;
+
+import com.jcraft.jsch.ChannelSftp;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+
+@Service
+@RequiredArgsConstructor
+public class SftpUploadService {
+
+    private final ChannelSftp channelSftp;
+
+    @Value("${sftp.remote-dir}")
+    private String remoteDir;
+
+    public String uploadFile(MultipartFile file) {
+        try (InputStream inputStream = file.getInputStream()) {
+            channelSftp.cd(remoteDir);
+            channelSftp.put(inputStream, file.getOriginalFilename());
+            return "File uploaded successfully to SFTP";
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to upload file: " + e.getMessage(), e);
+        }
     }
+}
