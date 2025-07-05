2d3ea65aed5dac5a18e7e062827f366d95db63b7
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-07-05 15:48:45 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,3 +1,13 @@
+private Dataset<Row> loadTxt(String path,  FileConfigDto fileConfigDto) {
+        Dataset<Row> df = sparkConfig.sparkSession().read()
+                .format("csv")
+                .option("header", false)
+                .option("delimiter", fileConfigDto.getDelimiter())  // Change if needed
+                .option("inferSchema", "true")
+                .load(path);
+        return mapColumn(df, fileConfigDto);
+    }
+
 Hash:4C62C790BF5D9814F4F661154C7AA8D52B9AEDDCF06F475E7AE50AF30588D456,Count:87,Amount:3954134.93
 id^atrnNum^debitAmt^date
 1^UAoHUrJQXz^3^10/07/2025
