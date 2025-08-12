ae4ef98c27b33f4ad407c0ac8df31cca14e72644
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-08-12 20:26:06 +0530

    Update spark.java

--- spark.java
+++ spark.java
@@ -1,36 +1,52 @@
-public static void main(String[] args) {
-    String jobId = String.valueOf(UUID.randomUUID());
-    MDC.put(CORRELATION, jobId);
-    MDC.put(SCENARIO, "ReconSparkAppMain");
-    MDC.put(OPERATION, "main");
-    String callbackUrl = args.length > 1 ? args[1] : "http://localhost:9097/api/rns/v1/spark/recon-complete-callback";
+private Map<String, Dataset<Row>> findMatchedUnmatchedAndDuplicateReconRecords(Dataset<Row> transactionDataset, Dataset<Row> reconFileDataset, UUID reconFileId) {
+        logger.info("Starting classification of recon data...");
+        final String reconFieldFormat = "recon.%s";
+        final String txnFieldFormat = "txn.%s";
+        // Step 3.1: Apply aliasing
+        Dataset<Row> recon = reconFileDataset.alias("recon");
+        Dataset<Row> txn = transactionDataset.alias("txn");
 
-    logger.info("Input args: {}", Arrays.toString(args));
-    UUID rfId;
-    if(StringUtils.isAllBlank(args)) {
-      logger.info("No rfId provided, args is all blank.");
-      callReconCallback(callbackUrl, jobId, FAILED, "Failed recon spark job.");
-      return;
-    }
-    try {
-      rfId = UUID.fromString(args[0]);
-    } catch (Exception e) {
-      logger.error("Invalid input rfId: {}", args[0]);
-      callReconCallback(callbackUrl, jobId, FAILED, "Failed recon spark job. No rfId provided");
-      return;
-    }
-    try (var context = new AnnotationConfigApplicationContext(SparkConfig.class)) {
-      logger.info("Application Context Created!!!");
-      SparkReconProcessingService service = context.getBean(SparkReconProcessingService.class);
-      service.reconProcessing(rfId);
-      callReconCallback(callbackUrl, jobId, "SUCCESS", "Completed recon spark job successfully.");
-      logger.info("ReconSparkApp Completed");
-    } catch (Exception ex) {
-      logger.error("Exception while the recon process, error message: {}", ex.getMessage());
-      callReconCallback(callbackUrl, jobId, FAILED, "Failed recon spark job, error message: " + ex.getMessage());
-    }
-  }
+        // Step 3.1.1: Apply row_number to tag first match per (ATRN_NUM, DEBIT_AMT, RFD_ID)
+        logger.info("Step-3.1: Apply row_number to tag first match per (ATRN_NUM, DEBIT_AMT, RFD_ID)");
+        WindowSpec matchWindow = Window.partitionBy(reconFieldFormat.formatted(OperationsConstant.ATRN_NUM), reconFieldFormat.formatted(OperationsConstant.DEBIT_AMT)).orderBy(reconFieldFormat.formatted(OperationsConstant.RFD_ID)); // can use any ordering
 
+        // Step 3.2: Perform left_outer join on ATRN_NUM + DEBIT_AMT
+        logger.info("Step-3.2: Performing join between recon and transaction data.");
+        Dataset<Row> joined = recon.join(txn, functions.col(reconFieldFormat.formatted(OperationsConstant.ATRN_NUM)).equalTo(functions.col(txnFieldFormat.formatted(OperationsConstant.ATRN_NUM))).and(functions.col(reconFieldFormat.formatted(OperationsConstant.DEBIT_AMT)).equalTo(functions.col(txnFieldFormat.formatted(OperationsConstant.DEBIT_AMT)))), "left_outer").withColumn(OperationsConstant.EXACT_MATCH, functions.when(functions.col(txnFieldFormat.formatted(OperationsConstant.ATRN_NUM)).isNotNull(), functions.lit(1)).otherwise(functions.lit(0))).withColumn(OperationsConstant.MATCH_RANK, functions.when(functions.col(OperationsConstant.EXACT_MATCH).equalTo(1), functions.row_number().over(matchWindow)));
 
- | 25/08/03 17:17:05 ERROR ReconSparkAppMain: Exception while the recon process, error message: Error creating bean with name 'sparkDataRepository' defined in URL [jar:file:/F:/spark/epay_operations_service/libs/recon-spark-job-0.1.0.jar!/com/epay/operations/recon/spark/repository/SparkDataRepository.class]: Unsatisfied dependency expressed through constructor parameter 0: Error creating bean with name 'sparkSession' defined in com.epay.operations.recon.spark.config.SparkConfig: Failed to instantiate [org.apache.spark.sql.SparkSession]: Factory method 'sparkSession' threw exception with message: java.net.UnknownHostException: namenode
-2025-08-03 17:17:06.809 INFO | com.epay.operations.controller.SparkController:72 | principal=  | scenario= | operation= | correlation= | reconCompleteCallback | Spark Recon Job Completed!!!
+        // Step 3.3: Find distinct matched ATRN_NUM
+        logger.info("Step-3.3: Find distinct matched ATRN_NUM.");
+        Dataset<Row> matchedAtrns = joined.filter(functions.col(OperationsConstant.MATCH_RANK).equalTo(1)).select(functions.col(reconFieldFormat.formatted(OperationsConstant.ATRN_NUM)).alias(OperationsConstant.MATCHED_ATRN)).distinct();
+
+        // Step 3.4: Re-join matched ATRN_NUM to mark duplicates (same ATRN_NUM, different DEBIT_AMT)
+        logger.info("Step-3.4: Re-join matched ATRN_NUM to mark duplicates");
+        Dataset<Row> withMatchedFlag = joined.join(matchedAtrns, functions.col(reconFieldFormat.formatted(OperationsConstant.ATRN_NUM)).equalTo(functions.col(OperationsConstant.MATCHED_ATRN)), "left_outer").withColumn(OperationsConstant.ATRN_MATCHED, functions.col(OperationsConstant.MATCHED_ATRN).isNotNull());
+
+        // Step 3.5: Apply classification logic
+        logger.info("Step-3.5: Applying classification logic to tag rows with status.");
+        Dataset<Row> finalStatus = withMatchedFlag.withColumn(OperationsConstant.RECON_STATUS, functions.when(functions.col(OperationsConstant.EXACT_MATCH).equalTo(1).and(functions.col(OperationsConstant.MATCH_RANK).equalTo(1)), functions.lit(OperationsConstant.RECON_STATUS_MATCHED)).when(functions.col(OperationsConstant.EXACT_MATCH).equalTo(1).and(functions.col(OperationsConstant.MATCH_RANK).gt(1)), functions.lit(OperationsConstant.RECON_STATUS_DUPLICATE)).when(functions.col(OperationsConstant.EXACT_MATCH).equalTo(0).and(functions.col(OperationsConstant.ATRN_MATCHED).equalTo(true)), functions.lit(OperationsConstant.RECON_STATUS_DUPLICATE)).otherwise(functions.lit(OperationsConstant.RECON_STATUS_UNMATCHED)));
+
+        // Step 3.6: Filter Dataset for the matched,unmatched and duplicate Recon Data
+        logger.info("Step-3.6: Filter Dataset for the matched,unmatched and duplicate Recon Data");
+        Dataset<Row> matched = finalStatus
+                .filter(functions.col(OperationsConstant.RECON_STATUS).equalTo(OperationsConstant.RECON_STATUS_MATCHED))
+                .withColumn(OperationsConstant.RECON_STATUS, functions.lit(OperationsConstant.RECON_STATUS_MATCHED))
+                .withColumn(OperationsConstant.SETTLEMENT_STATUS,
+                        functions.when(functions.col(OperationsConstant.SETTLEMENT_STATUS).isNull()
+                                        .or(functions.col(OperationsConstant.SETTLEMENT_STATUS).equalTo(Status.PENDING.name())),
+                                functions.lit(OperationsConstant.SETTLEMENT_STATUS_SETTLED)))
+                .withColumn(OperationsConstant.RF_ID, functions.lit(String.valueOf(reconFileId)))
+                .select(OperationsConstant.RFD_ID, OperationsConstant.RECON_STATUS, OperationsConstant.SETTLEMENT_STATUS, OperationsConstant.RF_ID);
+
+        Dataset<Row> unmatchedOrDuplicate = finalStatus
+                .filter(functions.col(OperationsConstant.RECON_STATUS).isin(OperationsConstant.RECON_STATUS_UNMATCHED, OperationsConstant.RECON_STATUS_DUPLICATE))
+                .withColumn(OperationsConstant.RF_ID, functions.lit(String.valueOf(reconFileId)))
+                .select(OperationsConstant.RFD_ID, OperationsConstant.RECON_STATUS, OperationsConstant.RF_ID);
+
+        // Step 3.7: Return as Map data.
+        Map<String, Dataset<Row>> result = new HashMap<>();
+        result.put(OperationsConstant.RECON_STATUS_MATCHED, matched);
+        result.put(OperationsConstant.OTHERS, unmatchedOrDuplicate);
+
+        return result;
+    }
