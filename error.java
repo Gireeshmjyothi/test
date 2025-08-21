53b7e2bce4e5a8bb98c946c2279dfd8d3ed202f1
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-08-21 18:53:35 +0530

    Update error.java

--- error.java
+++ error.java
@@ -1,22 +1,24 @@
-public Dataset<Row> readAndNormalizeTransaction(UUID rfId) {
-        String query = "MERCHANT_TXN"; // buildQueryForTransaction(rfId);
-        logger.info("Executing query for : {}", query);
-        return normalize(readFromDBWithFilter(query));
-    }
+Dataset<Row> df = dataset.persist(); // if reused
 
-    /**
-     * This method is used to normalise the dataset columns.
-     *
-     * @param dataset required dataset.
-     * @return normalised dataset.
-     */
-    public Dataset<Row> normalize(Dataset<Row> dataset) {
-        logger.info("Normalising required data.");
-        var columns = Arrays.asList(dataset.columns());
-        for (String col : MAPPING_COLUMNS.keySet()) {
-            if (columns.contains(col)) {
-                dataset = dataset.withColumn(col, functions.trim(dataset.col(col)));
-            }
-        }
-        return dataset;
-    }
+long[] bounds = df.agg(expr("min(RFD_ID) AS mn"), expr("max(RFD_ID) AS mx"))
+                  .as(Encoders.tuple(Encoders.LONG(), Encoders.LONG()))
+                  .first();
+
+df = df.repartition(96, functions.col("RFD_ID")); // match cluster parallelism
+
+df.write()
+  .format("jdbc")
+  .option("url", System.getProperty("dbUrl"))
+  .option("user", System.getProperty("dbUser"))
+  .option("password", System.getProperty("dbPassword"))
+  .option("driver", "oracle.jdbc.OracleDriver")
+  .option("dbtable", tableName)
+  .option("batchsize", "50000")
+  .option("isolationLevel", "NONE")
+  .option("partitionColumn", "RFD_ID")
+  .option("lowerBound", String.valueOf(bounds[0]))
+  .option("upperBound", String.valueOf(bounds[1]))
+  .option("numPartitions", "96")
+  .option("sessionInitStatement", "ALTER SESSION ENABLE PARALLEL DML")
+  .mode(SaveMode.Append)
+  .save();
