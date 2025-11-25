86f987dae0e081eb64362c2146652eca40c25fa6
Gireesh M <109240951+Gireeshmjyothi@users.noreply.github.com>
2025-11-25 21:00:56 +0530

    Update spark.java

--- spark.java
+++ spark.java
@@ -1,16 +1,21 @@
 private List<EGrasSuccessResponse> buildOrderStatusResponse(List<OrderStatusResponse> orderStatusResponseList) {
 
     return orderStatusResponseList.stream()
-            .flatMap(orderStatus -> orderStatus.getPaymentInfoList().stream()
-                    .map(payment -> EGrasSuccessResponse.builder()
-                            .bid(payment.getBid())
-                            .prn(orderStatus.getOrderInfo().getPrn())
-                            .grn(payment.getGrn())
-                            .amt(payment.getAmount())
-                            .payStatus(payment.getPayStatus())
-                            .transCompletionDateTime(payment.getTransactionDateTime())
-                            .build()
-                    )
-            )
+            .flatMap(orderStatus -> {
+
+                OrderInfo orderInfo = orderStatus.getOrderInfo();
+
+                return orderStatus.getPaymentInfoList().stream().map(payment -> 
+                        EGrasSuccessResponse.builder()
+                                .bid(payment.getBid())                       // From PaymentInfo
+                                .prn(orderInfo.getPrn())                     // From OrderInfo
+                                .grn(payment.getGrn())                       // From PaymentInfo
+                                .amt(payment.getAmount())                    // From PaymentInfo
+                                .payStatus(payment.getPayStatus())           // From PaymentInfo
+                                .transCompletionDateTime(payment.getTxnDate()) // From PaymentInfo
+                                // add more mapping as needed
+                                .build()
+                );
+            })
             .collect(Collectors.toList());
 }
